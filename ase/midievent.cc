614d8f08a0afe33c0fc146d4c44d88fe22243d36
Tim Janik <timj@gnu.org>
2021-02-25 02:27:35 +0100

    ASE: midievent: provide event definition for MIDI message exchange
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ ase/midievent.cc
@@ -0,0 +1,207 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+#include "ase/midievent.hh"
+#include "internal.hh"
+
+#define EDEBUG(...)     Ase::debug ("event", __VA_ARGS__)
+
+namespace Ase {
+
+// == MidiEvent ==
+MidiEvent::MidiEvent (const MidiEvent &other) :
+  MidiEvent()
+{
+  *this = other;
+}
+
+MidiEvent::MidiEvent (MidiEventType etype)
+{
+  memset ((void*) this, 0, sizeof (*this));
+  type = etype;
+  // one main design consideration is minimized size
+  static_assert (sizeof (MidiEvent) <= 2 * sizeof (void*));
+}
+
+MidiEvent&
+MidiEvent::operator= (const MidiEvent &other)
+{
+  if (this != &other)
+    memcpy ((void*) this, &other, sizeof (*this));
+  return *this;
+}
+
+MidiEvent::~MidiEvent ()
+{}
+
+/// Determine extended message type an MidiEvent.
+MidiMessage
+MidiEvent::message () const
+{
+  if (type == MidiEvent::CONTROL_CHANGE)
+    {
+      if (param >= uint (MidiMessage::ALL_SOUND_OFF) &&
+          param <= uint (MidiMessage::POLY_MODE_ON))
+        return MidiMessage (param);
+    }
+  return MidiMessage (type);
+}
+
+std::string
+MidiEvent::to_string () const
+{
+  const char *et = nullptr;
+  switch (type)
+    {
+    case NOTE_OFF:        if (!et) et = "NOTE_OFF";
+    case NOTE_ON:         if (!et) et = "NOTE_ON";
+    case AFTERTOUCH:      if (!et) et = "AFTERTOUCH";
+      return string_format ("%+4d ch=%-2u %-10s pitch=%d vel=%f tune=%f id=%x",
+                            frame, channel, et, key, velocity, tuning, noteid);
+    case CONTROL_CHANGE:        if (!et) et = "CONTROL_CHANGE";
+      return string_format ("%+4d ch=%-2u %s control=%d value=%f (%02x) {%u}",
+                            frame, channel, et, param, value, cval, fragment);
+    case PROGRAM_CHANGE:        if (!et) et = "PROGRAM_CHANGE";
+      return string_format ("%+4d ch=%-2u %s program=%d",
+                            frame, channel, et, param);
+    case CHANNEL_PRESSURE:      if (!et) et = "CHANNEL_PRESSURE";
+    case PITCH_BEND:            if (!et) et = "PITCH_BEND";
+      return string_format ("%+4d ch=%-2u %s value=%+f",
+                            frame, channel, et, value);
+    case SYSEX:                 if (!et) et = "SYSEX";
+      return string_format ("%+4d %s (unhandled)", frame, et);
+    default:
+      return string_format ("%+4d MidiEvent-%u (unhandled)", frame, type);
+    }
+}
+
+MidiEvent
+make_note_on (uint16 chnl, uint8 mkey, float velo, float tune, uint nid)
+{
+  MidiEvent ev (velo > 0 ? MidiEvent::NOTE_ON : MidiEvent::NOTE_OFF);
+  ev.channel = chnl;
+  ev.key = mkey;
+  ev.velocity = velo;
+  ev.tuning = tune;
+  ev.noteid = nid;
+  return ev;
+}
+
+MidiEvent
+make_note_off (uint16 chnl, uint8 mkey, float velo, float tune, uint nid)
+{
+  MidiEvent ev (MidiEvent::NOTE_OFF);
+  ev.channel = chnl;
+  ev.key = mkey;
+  ev.velocity = velo;
+  ev.tuning = tune;
+  ev.noteid = nid;
+  return ev;
+}
+
+MidiEvent
+make_aftertouch (uint16 chnl, uint8 mkey, float velo, float tune, uint nid)
+{
+  MidiEvent ev (MidiEvent::AFTERTOUCH);
+  ev.channel = chnl;
+  ev.key = mkey;
+  ev.velocity = velo;
+  ev.tuning = tune;
+  ev.noteid = nid;
+  return ev;
+}
+
+MidiEvent
+make_pressure (uint16 chnl, float velo)
+{
+  MidiEvent ev (MidiEvent::CHANNEL_PRESSURE);
+  ev.channel = chnl;
+  ev.velocity = velo;
+  return ev;
+}
+
+MidiEvent
+make_control (uint16 chnl, uint prm, float val)
+{
+  MidiEvent ev (MidiEvent::CONTROL_CHANGE);
+  ev.channel = chnl;
+  ev.param = prm;
+  ev.value = val;
+  ev.cval = ev.value * 127;
+  return ev;
+}
+
+MidiEvent
+make_control8 (uint16 chnl, uint prm, uint8 cval)
+{
+  MidiEvent ev (MidiEvent::CONTROL_CHANGE);
+  ev.channel = chnl;
+  ev.param = prm;
+  ev.cval = cval;
+  ev.value = ev.cval * (1.0 / 127.0);
+  return ev;
+}
+
+MidiEvent
+make_program (uint16 chnl, uint prgrm)
+{
+  MidiEvent ev (MidiEvent::PROGRAM_CHANGE);
+  ev.channel = chnl;
+  ev.param = prgrm;
+  return ev;
+}
+
+MidiEvent
+make_pitch_bend (uint16 chnl, float val)
+{
+  MidiEvent ev (MidiEvent::PITCH_BEND);
+  ev.channel = chnl;
+  ev.value = val;
+  return ev;
+}
+
+// == MidiEventStream ==
+MidiEventStream::MidiEventStream ()
+{
+  events_.reserve (16);
+}
+
+/// Append an MidiEvent with conscutive `frame` time stamp.
+void
+MidiEventStream::append (int8_t frame, const MidiEvent &event)
+{
+  const bool out_of_order_event = append_unsorted (frame, event);
+  assert_return (!out_of_order_event);
+}
+
+/// Dangerous! Append an MidiEvent with enforcing sort order, violates constraints.
+/// Returns if ensure_order() must be called due to adding an out-of-order event.
+bool
+MidiEventStream::append_unsorted (int8_t frame, const MidiEvent &event)
+{
+  const int64_t last_event_stamp = !events_.empty() ? events_.back().frame : -128;
+  events_.push_back (event);
+  events_.back().frame = frame;
+  return frame < last_event_stamp;
+}
+
+/// Fix event order after append_unsorted() returned `true`.
+void
+MidiEventStream::ensure_order ()
+{
+  std::stable_sort (events_.begin(), events_.end(), [] (const MidiEvent &a, const MidiEvent &b) -> bool {
+    return a.frame < b.frame;
+  });
+}
+
+/// Fetch the latest event stamp, can be used to enforce order.
+int64_t
+MidiEventStream::last_frame () const
+{
+  return !events_.empty() ? events_.back().frame : -128;
+}
+
+// == MidiEventRange ==
+MidiEventRange::MidiEventRange (const MidiEventStream &estream) :
+  estream_ (estream)
+{}
+
+} // Ase
---
+++ ase/midievent.hh
@@ -0,0 +1,112 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+#ifndef __ASE_MIDI_EVENT_HH__
+#define __ASE_MIDI_EVENT_HH__
+
+#include <ase/memory.hh>
+
+namespace Ase {
+
+/// Type of MIDI Events.
+enum class MidiEventType : uint8_t {};
+
+/// Extended type information for MidiEvent.
+enum class MidiMessage : int32_t {
+  NONE                          = 0,
+  ALL_SOUND_OFF                 = 120,
+  RESET_ALL_CONTROLLERS         = 121,
+  LOCAL_CONTROL                 = 122,
+  ALL_NOTES_OFF                 = 123,
+  OMNI_MODE_OFF                 = 124,
+  OMNI_MODE_ON                  = 125,
+  MONO_MODE_ON                  = 126,
+  POLY_MODE_ON                  = 127,
+  NOTE_OFF                      = 0x80,
+  NOTE_ON                       = 0x90,
+  AFTERTOUCH                    = 0xA0,
+  CONTROL_CHANGE                = 0xB0,
+  PROGRAM_CHANGE                = 0xC0,
+  CHANNEL_PRESSURE              = 0xD0,
+  PITCH_BEND                    = 0xE0,
+  SYSEX                         = 0xF0,
+};
+
+/// MidiEvent data structure.
+struct MidiEvent {
+  constexpr static MidiEventType NOTE_OFF         = MidiEventType (0x80);
+  constexpr static MidiEventType NOTE_ON          = MidiEventType (0x90);
+  constexpr static MidiEventType AFTERTOUCH       = MidiEventType (0xA0); ///< Key Pressure, polyphonic aftertouch
+  constexpr static MidiEventType CONTROL_CHANGE   = MidiEventType (0xB0); ///< Control Change
+  constexpr static MidiEventType PROGRAM_CHANGE   = MidiEventType (0xC0);
+  constexpr static MidiEventType CHANNEL_PRESSURE = MidiEventType (0xD0); ///< Channel Aftertouch
+  constexpr static MidiEventType PITCH_BEND       = MidiEventType (0xE0);
+  constexpr static MidiEventType SYSEX            = MidiEventType (0xF0);
+  MidiEventType type;   ///< MidiEvent type, one of the MidiEventType members
+  int8      frame;      ///< Offset into current block, delayed if negative
+  uint8     channel;    ///< 1…16 for standard events
+  union {
+    uint8   key;        ///< NOTE, KEY_PRESSURE MIDI note, 0…0x7f, 60 = middle C at 261.63 Hz.
+    uint8   fragment;   ///< Flag for multi-part control change mesages.
+  };
+  union {
+    uint    length;     ///< Data event length of byte array.
+    uint    param;      ///< PROGRAM_CHANGE program, CONTROL_CHANGE controller, 0…0x7f
+    uint    noteid;     ///< NOTE, identifier for note expression handling or 0xffffffff.
+  };
+  union {
+    char   *data;       ///< Data event byte array.
+    struct {
+      float value;      ///< CONTROL_CHANGE 0…+1, CHANNEL_PRESSURE, 0…+1, PITCH_BEND -1…+1
+      uint  cval;       ///< CONTROL_CHANGE control value, 0…0x7f
+    };
+    struct {
+      float velocity;   ///< NOTE, KEY_PRESSURE, CHANNEL_PRESSURE, 0…+1
+      float tuning;     ///< NOTE, fine tuning in ±cents
+    };
+  };
+  explicit    MidiEvent (MidiEventType etype = MidiEventType (0));
+  /*copy*/    MidiEvent (const MidiEvent &other);
+  MidiEvent&  operator= (const MidiEvent &other);
+  /*des*/    ~MidiEvent ();
+  MidiMessage message   () const;
+  std::string to_string () const;
+};
+
+MidiEvent make_note_on    (uint16 chnl, uint8 mkey, float velo, float tune = 0, uint nid = 0xffffff);
+MidiEvent make_note_off   (uint16 chnl, uint8 mkey, float velo, float tune = 0, uint nid = 0xffffff);
+MidiEvent make_aftertouch (uint16 chnl, uint8 mkey, float velo, float tune = 0, uint nid = 0xffffff);
+MidiEvent make_pressure   (uint16 chnl, float velo);
+MidiEvent make_control    (uint16 chnl, uint prm, float val);
+MidiEvent make_control8   (uint16 chnl, uint prm, uint8 cval);
+MidiEvent make_program    (uint16 chnl, uint prgrm);
+MidiEvent make_pitch_bend (uint16 chnl, float val);
+
+/// A stream of writable MidiEvent structures.
+class MidiEventStream {
+  std::vector<MidiEvent> events_; // TODO: use O(1) allocator
+  friend class MidiEventRange;
+public:
+  explicit         MidiEventStream ();
+  void             append          (int8_t frame, const MidiEvent &event);
+  const MidiEvent* begin           () const noexcept { return &*events_.begin(); }
+  const MidiEvent* end             () const noexcept { return &*events_.end(); }
+  size_t           size            () const noexcept { return events_.size(); }
+  bool             empty           () const noexcept { return events_.empty(); }
+  void             clear           () noexcept       { events_.clear(); }
+  bool             append_unsorted (int8_t frame, const MidiEvent &event);
+  void             ensure_order    ();
+  int64_t          last_frame      () const ASE_PURE;
+};
+
+/// A readonly view and iterator into an MidiEventStream.
+class MidiEventRange {
+  const MidiEventStream &estream_;
+public:
+  const MidiEvent* begin          () const  { return &*estream_.begin(); }
+  const MidiEvent* end            () const  { return &*estream_.end(); }
+  size_t           events_pending () const  { return estream_.size(); }
+  explicit         MidiEventRange (const MidiEventStream &estream);
+};
+
+} // Ase
+
+#endif // __ASE_MIDI_EVENT_HH__
