96f58786aba60cba5f3da5ad21b8956b3abc14c4
Tim Janik <timj@gnu.org>
2021-02-25 02:30:00 +0100

    ASE: datautils: add helpers to fill and convert float blocks
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ ase/datautils.cc
@@ -0,0 +1,8 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+#include "datautils.hh"
+
+namespace Ase {
+
+float const_float_zeros[AUDIO_BLOCK_FLOAT_ZEROS_SIZE] = { 0, /*...*/ };
+
+} // Ase
