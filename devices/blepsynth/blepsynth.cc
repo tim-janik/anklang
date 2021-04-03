e1159a8f5af322314a7f9e3071eb1b6925ead08d
Tim Janik <timj@gnu.org>
2021-04-03 04:20:06 +0200

    DEVICES: blepsynth/: relicense to MPL-2.0
    Code originally by:
      Stefan Westerfeld <stefan@space.twc.de>
    
    License statement to use the code under MPL-2:
      https://mail.gnome.org/archives/beast/2021-February/msg00001.html
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- devices/blepsynth/blepsynth.cc
+++ devices/blepsynth/blepsynth.cc
@@ -1,4 +1,4 @@
-// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 #include "ase/processor.hh"
 #include "ase/signalmath.hh"
 #include "ase/asenote.hh"
--- devices/blepsynth/plotblep.py
+++ devices/blepsynth/plotblep.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Licensed GNU LGPL v3 or later: http://www.gnu.org/licenses/lgpl.html
+# This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 
 # coding=utf-8
 
