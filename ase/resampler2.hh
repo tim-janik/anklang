17db2f8abae5c70e72defaa948b07bd07514f421
Tim Janik <timj@gnu.org>
2021-04-03 04:00:13 +0200

    ASE: resampler2: relicense to MPL-2.0
    Code originally by:
      Stefan Westerfeld <stefan@space.twc.de>
    
    License statement to use the code under MPL-2:
      https://mail.gnome.org/archives/beast/2021-February/msg00001.html
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ase/resampler2.hh
+++ ase/resampler2.hh
@@ -1,4 +1,4 @@
-// Licensed GNU LGPL v2.1 or later: http://www.gnu.org/licenses/lgpl.html
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 #ifndef __BSE_RESAMPLER_HH__
 #define __BSE_RESAMPLER_HH__
 
