5a3875be78fb9175ddf4d8f773cd0647057f91bc
Tim Janik <timj@gnu.org>
2021-05-01 14:14:24 +0200

    UI: b/menubar.vue: enable Open, Save, Save-As
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/b/menubar.vue
+++ ui/b/menubar.vue
@@ -33,10 +33,10 @@
 	  <b-icon bc="menumore" />
 	</div>
 	<b-contextmenu ref="filemenu" @click="activation" startfocus keepmounted >
-	  <b-menuitem fa="file-o"	kbd="Ctrl+KeyN"		uri="loadnew"			   >	New Project		</b-menuitem>
-	  <b-menuitem fa="file-audio-o" kbd="Ctrl+KeyO"		uri="load"   :disabled="!window.Electron" >	Open Project…		</b-menuitem>
-	  <b-menuitem mi="save_alt"     kbd="Ctrl+KeyS"		uri="save"   :disabled="!window.Electron" >	Save Project		</b-menuitem>
-	  <b-menuitem fa="save"		kbd="Shift+Ctrl+KeyS"	uri="saveas" :disabled="!window.Electron" >	Save As…		</b-menuitem>
+	  <b-menuitem fa="file-o"	kbd="Ctrl+KeyN"		uri="loadnew" >	New Project		</b-menuitem>
+	  <b-menuitem fa="file-audio-o" kbd="Ctrl+KeyO"		uri="load"    >	Open Project…		</b-menuitem>
+	  <b-menuitem mi="save_alt"     kbd="Ctrl+KeyS"		uri="save"    >	Save Project		</b-menuitem>
+	  <b-menuitem fa="save"		kbd="Shift+Ctrl+KeyS"	uri="saveas"  >	Save As…		</b-menuitem>
 	  <b-menuseparator style="margin: 7px"  />
 	  <b-menuitem fa="cog"         kbd="Ctrl+Comma"	uri="prefs">	Preferences		</b-menuitem>
 	  <b-menuseparator style="margin: 7px"  />
@@ -137,10 +137,10 @@ export default {
 	    document.body.requestFullscreen();
 	  break;
 	case 'loadnew':
-	  App.load_project();
+	  App.load_project_checked();
 	  break;
 	case 'load':
-	  openfile();
+	  open_file();
 	  break;
 	case 'save':
 	  save_project();
@@ -153,98 +153,73 @@ export default {
   },
 };
 
-let open_dialog_lastdir = undefined;
-
-async function openfile()
-{
-  if (!window.Electron)
-    return;
-  const Electron = window.Electron;
-  if (!open_dialog_lastdir)
-    open_dialog_lastdir = await Ase.server.get_demo_path();
+async function open_file() {
+  //if (!open_file.last_dir)
+  //  open_file.last_dir = await (await Ase.server.cwd_crawler()).get_dir ('home');
   const opt = {
-    title: Util.format_title ('Anklang', 'Select File To Open'),
-    buttonLabel: 'Open File',
-    defaultPath: open_dialog_lastdir,
-    properties: ['openFile', ], // 'multiSelections' 'openDirectory' 'showHiddenFiles'
-    filters: [ { name: 'Projects', extensions: ['anklang'] },
+    title:  Util.format_title ('Anklang', 'Select File To Open'),
+    button: 'Open File',
+    cwd:    open_file.last_dir,
+    filters: [ { name: 'Projects', extensions: ['anklang'] }, // TODO: filters
 	       { name: 'Audio Files', extensions: [ 'anklang', 'mid', 'wav', 'mp3', 'ogg' ] },
 	       { name: 'All Files', extensions: [ '*' ] }, ],
   };
-  const dresult = await Electron.dialog.showOpenDialog (Electron.getCurrentWindow(), opt);
-  if (!dresult.canceled && dresult.filePaths.length == 1)
+  const filename = await App.shell.select_file (opt);
+  if (filename)
     {
-      const filename = dresult.filePaths[0];
-      open_dialog_lastdir = filename.replace (/.*[\/\\]/, ''); // dirname
-      const lresult = await App.load_project (filename);
+      open_file.last_dir = filename.replace (/[^\/]*$/, ''); // dirname
+      const lresult = await App.load_project_checked (filename);
       if (lresult == Ase.Error.NONE)
 	{
 	  let project_filename = filename;
           Data.project.set_custom ('filename', project_filename);
 	}
-      else
-	console.error ('Failed to load:', filename); // FIXME : warning
+      // else console.error ('Failed to load:', filename); // FIXME : warning
     }
 }
 
-let save_dialog_lastdir = undefined;
-
-async function save_project (asnew = false)
-{
-  if (!window.Electron)
+async function save_project (asnew = false) {
+  const opt = {
+    title:  Util.format_title ('Anklang', 'Save Project'),
+    button: 'Save As',
+    cwd:    save_project.last_dir,
+    filters: [ { name: 'Projects', extensions: ['anklang'] }, ],
+  };
+  let filename = ''; // TODO: await Data.project.get_custom ('filename');
+  let replace = asnew ? 0 : !!filename;
+  if (asnew || !filename)
+    filename = await App.shell.select_file (opt);
+  if (!filename)
     return false;
-  const Electron = window.Electron;
-  const Path = require ('path');
-  if (!save_dialog_lastdir)
-    save_dialog_lastdir = Electron.app.getPath ('music') || Path.resolve ('.');
-  let savepath = asnew ? '' : await Data.project.get_custom ('filename');
-  if (!savepath)
+  save_project.last_dir = filename.replace (/[^\/]*$/, ''); // dirname
+  if (!filename.endsWith ('.anklang'))
+    filename += '.anklang';
+  if (!replace) // TODO: if file_exists (filename)
     {
-      const opt = {
-	title: Util.format_title ('Anklang', 'Save Project'),
-	buttonLabel: 'Save As',
-	defaultPath: save_dialog_lastdir,
-	properties: [], // 'showOverwriteConfirmation'
-	filters: [ { name: 'Projects', extensions: ['anklang'] }, ],
-      };
-      const dresult = await Electron.dialog.showSaveDialog (Electron.getCurrentWindow(), opt);
-      if (dresult.canceled || !dresult.filePath)
-	return false;
-      savepath = dresult.filePath;
-    }
-  const Fs = require ('fs');
-  if (!savepath.endsWith ('.anklang'))
-    {
-      savepath += '.anklang';
-      let replace = 1;
-      if (Fs.existsSync (savepath))
-	replace = await App.async_modal_dialog ("Replace Project?",
-						"Replace existing project file?\n" +
-						savepath + ": File exists",
-						[ 'Cancel', { label: 'Replace', autofocus: true } ],
-						'QUESTION');
+      replace = await App.async_modal_dialog ("Replace Project?",
+					      "Replace existing project file?\n" +
+					      filename + ": File exists", [
+						'Cancel',
+						{ label: 'Replace', autofocus: true }, ],
+					      'QUESTION');
       if (replace != 1)
 	return false;
     }
-  save_dialog_lastdir = Path.dirname (savepath);
-  if (Fs.existsSync (savepath))
-    Fs.unlinkSync (savepath);
-  const err = await App.save_project (savepath);
-  if (err == Ase.Error.NONE)
+  const err = await App.save_project (filename);
+  if (err === Ase.Error.NONE)
     {
-      Data.project.set_custom ('filename', savepath);
+      Data.project.set_custom ('filename', filename);
       let msg = '# Project Saved\n';
-      msg += '  \n  \nProject saved to: ``' + savepath + '``\n';
+      msg += '  \n  \nProject saved to: ``' + filename + '``\n';
       Util.show_note (msg);
       return true;
     }
   App.async_modal_dialog ("File IO Error",
 			  "Failed to save project.\n" +
-			  savepath + ": " +
-			  await Ase.server.describe_error (err),
-			  [ { label: 'Dismiss', autofocus: true } ],
-			  'ERROR');
-  // <b-icon fa="warning" style="display: none; font-size: 250%; padding-right: 1em; float: left; color: #d92" />
+			  filename + ": " +
+			  await Ase.server.error_blurb (err), [
+			    { label: 'Dismiss', autofocus: true }
+			  ], 'ERROR');
   return false;
 }
 
