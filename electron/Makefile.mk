8283a5233978a992804391f3f163d9e8fb49769e
Tim Janik <timj@gnu.org>
2021-05-02 02:27:19 +0200

    ELECTRON: Makefile.mk: change Electron app name to "anklang/electron"
    The name change is not directly user visible, because index.html sets
    a <title/> tag. It does change the Electron data/cache directory to
    ~/.config/anklang/electron/, so electron data now resides under the
    ~/.config/anklang/ directory which is used anyway. And it changes the
    X11 WM_CLASS string to "anklang/electron".
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- electron/Makefile.mk
+++ electron/Makefile.mk
@@ -16,7 +16,7 @@ $>/electron/anklang: $(electron/js.sources) electron/Makefile.mk $>/node_modules
 	$Q mkdir -p $(@D)/resources/app
 	$Q $(CP) $(electron/js.sources) $(@D)/resources/app/
 	$Q echo '{ "private": true,'					>  $(@D)/resources/app/package.json
-	$Q echo '  "name": "Anklang",'					>> $(@D)/resources/app/package.json
+	$Q echo '  "name": "anklang/electron",'				>> $(@D)/resources/app/package.json
 	$Q echo '  "version": "$(version_m.m.m)",'			>> $(@D)/resources/app/package.json
 	$Q echo '  "main": "main.js",'					>> $(@D)/resources/app/package.json
 	$Q echo '  "mode": "$(MODE)" }'					>> $(@D)/resources/app/package.json
