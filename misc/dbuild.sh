80c8b3458c912cbe8c3fc2e8a45538ec64ee5e7c
Tim Janik <timj@gnu.org>
2021-06-06 23:41:12 +0200

    MISC: dbuild.sh: allow -d <DIST> and rename misc/.dbuild/.cache/
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/dbuild.sh
+++ misc/dbuild.sh
@@ -5,18 +5,24 @@ set -Eeuo pipefail
 SCRIPTNAME=${0##*/} ; die() { [ -z "$*" ] || echo "$SCRIPTNAME: $*" >&2; exit 128 ; }
 
 # == defaults ==
+PROJECT=anklang
+DOCKERFILE=misc/Dockerfile-apt
+DIST=ubuntu:focal
 DOCKEROPTIONS="--cap-add SYS_PTRACE"
-DOCKERDIST=focal
-INITIALIZE=
 EXEC_CMD=
+INITIALIZE=false
+IMGTAG=$PROJECT-dbuild
+docker images | grep -q "^$IMGTAG " || INITIALIZE=true
 
 # == usage ==
 usage() {
   #     12345678911234567892123456789312345678941234567895123456789612345678971234567898
   echo "Usage: $0 [OPTIONS] [--] [COMMAND...]"
-  echo "  -f <DIST>         Choose a distribution"
+  echo "Run COMMAND in $PWD inside docker environment."
+  echo "  -d <DIST>         Set DIST build arg [$DIST]"
+  echo "  -f <DOCKERFILE>   Choose a Dockerfile [$DOCKERFILE]"
   echo "  -h                Display command line help"
-  echo "  -i                Initialize docker build environment"
+  echo "  -i                Initialize docker build environment [$INITIALIZE]"
   echo "  shell             COMMAND: Run shell"
   echo "  root              COMMAND: Run root shell"
 }
@@ -24,7 +30,8 @@ usage() {
 # == parse args ==
 while test $# -ne 0 ; do
   case "$1" in \
-    -f)		shift; DOCKERDIST="$1" ;;
+    -d)		shift; DIST="$1" ;;
+    -f)		shift; DOCKERFILE="$1" ;;
     -h)		usage ; exit 0 ;;
     -i)		INITIALIZE=true ;;
     --)		shift ; break ;;
@@ -36,45 +43,40 @@ test $# -ne 0 || { usage ; exit 0 ; }
 EXEC_CMD="$1" && shift
 
 # == Detect User Settings ==
-DOCKERFILE=misc/Dockerfile-$DOCKERDIST
 TUID=`id -u`
 TGID=`id -g`
-ANKLANG_USER_VOLUME="--user $TUID:$TGID -v `pwd`:/usr/src/anklang/"
+PROJECT_VOLUME="--user $TUID:$TGID -v `pwd`:/$PROJECT/"
 
 # == Initialize Build Environment ==
-test -z "$INITIALIZE" || {
-  mkdir -p misc/.cicache/electron/
-  test ! -d ~/.cache/electron/. || cp --reflink=auto --preserve=timestamps ~/.cache/electron/. -r misc/.cicache/electron/
+$INITIALIZE && {
+  mkdir -p misc/.dbuild/.cache/
+  test ! -d ~/.cache/electron/. || cp --reflink=auto --preserve=timestamps ~/.cache/electron -r misc/.dbuild/.cache/
   ( set -x
-    docker build -f "$DOCKERFILE" \
-	   --build-arg USERGROUP="$TUID:$TGID" \
-	   -t anklang-cbuild misc/
+    docker build -f "$DOCKERFILE" --build-arg DIST="$DIST" --build-arg USERGROUP="$TUID:$TGID" -t $IMGTAG misc/
   )
-  rm -r misc/.cicache/
+  rm -f -r misc/.dbuild/
 }
 
 # == Keep interactive tty ==
 tty >/dev/null && TI=-ti || TI=-t
 
 # == Copy CWD to temporary volume ==
-setup_ANKLANG_TEMP_VOLUME() {
-  test -n "${TMPVOL:-}" || {
-    TMPVOL=anklang-cbuild-tmpvol
-    trap "docker volume rm $TMPVOL >/dev/null" 0 HUP INT QUIT TRAP USR1 PIPE TERM ERR EXIT
-    docker run $DOCKEROPTIONS -v `pwd`:/usr_src_anklang/:ro -v $TMPVOL:/usr/src/anklang/ $TI --rm anklang-cbuild \
-	   cp -a --reflink=auto /usr_src_anklang/. /usr/src/anklang/
+setup_ROOT_TMPVOLUME() {
+  test -n "${TMPVOLUME:-}" || {
+    TMPVOLUME=$PROJECT-tmpvol
+    trap "docker volume rm $TMPVOLUME >/dev/null" 0 HUP INT QUIT TRAP USR1 PIPE TERM ERR EXIT
+    docker run $DOCKEROPTIONS -v `pwd`:/pwd_rovolume/:ro -v $TMPVOLUME:/$PROJECT/ $TI --rm $IMGTAG \
+	   cp -a --reflink=auto /pwd_rovolume/. /$PROJECT/
   }
-  ANKLANG_TEMP_VOLUME="-v $TMPVOL:/usr/src/anklang/"
+  DOCKER_RUN_VOLUME="-v $TMPVOLUME:/$PROJECT/"
 }
 
-# == Shell ==
-ANKLANG_TEMP_VOLUME="$ANKLANG_USER_VOLUME"
-test root != "$EXEC_CMD" || setup_ANKLANG_TEMP_VOLUME
-test root != "$EXEC_CMD" -a shell != "$EXEC_CMD" ||
-  EXEC_CMD=/bin/bash
+# == EXEC_CMD ==
+DOCKER_RUN_VOLUME="$PROJECT_VOLUME"
+test root != "$EXEC_CMD" || setup_ROOT_TMPVOLUME # let root operate on a temporary copy
+test root != "$EXEC_CMD" -a shell != "$EXEC_CMD" || EXEC_CMD=/bin/bash
 test -z "$EXEC_CMD" || (
   set -x
-  docker run $DOCKEROPTIONS $ANKLANG_TEMP_VOLUME $TI --rm anklang-cbuild \
-	 "$EXEC_CMD" "$@"
-) # avoid 'exec' to carry out the setup_ANKLANG_TEMP_VOLUME trap
+  docker run $DOCKEROPTIONS $DOCKER_RUN_VOLUME $TI --rm -w /$PROJECT/ $IMGTAG "$EXEC_CMD" "$@"
+) # avoid 'exec' for TRAP(1) to take effect
 exit $?
