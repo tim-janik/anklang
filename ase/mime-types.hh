a6bba376301f4abda612eafbda6a1fcda6f354b3
Tim Janik <timj@gnu.org>
2021-08-16 00:40:26 +0200

    ASE: mime-types.hh: sort extensions
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ase/mime-types.hh
+++ ase/mime-types.hh
@@ -7,19 +7,19 @@ application/javascript js mjs
 application/json json
 application/ogg ogx
 application/pdf pdf
-application/postscript ps ai eps epsi epsf eps2 eps3
+application/postscript ai eps eps2 eps3 epsf epsi ps
 application/wasm wasm
 application/x-bittorrent torrent
 application/x-debian-package deb udeb
-application/x-font pfa pfb gsf
+application/x-font gsf pfa pfb
 application/x-gtar gtar
-application/x-gtar-compressed tgz taz
+application/x-gtar-compressed taz tgz
 application/x-iso9660-image iso
 application/x-redhat-package-manager rpm
 application/x-rss+xml rss
 application/x-tar tar
 application/x-xz xz
-application/xhtml+xml xhtml xht
+application/xhtml+xml xht xhtml
 application/xml xml xsd
 application/zip zip
 audio/adpcm adp
@@ -27,16 +27,16 @@ audio/amr amr
 audio/amr-wb awb
 audio/annodex axa
 audio/basic au snd
-audio/midi mid midi kar rmi
+audio/midi kar mid midi rmi
 audio/mp4 m4a mp4a
-audio/mpeg mpga mpega mp2 mp2a mp3 m2a m3a
+audio/mpeg m2a m3a mp2 mp2a mp3 mpega mpga
 audio/ogg oga ogg opus spx
 audio/prs.sid sid
 audio/s3m s3m
 audio/silk sil
 audio/webm weba
 audio/x-aac aac
-audio/x-aiff aif aiff aifc
+audio/x-aiff aif aifc aiff
 audio/x-caf caf
 audio/x-csound csd orc sco
 audio/x-flac flac
@@ -45,7 +45,7 @@ audio/x-matroska mka
 audio/x-mpegurl m3u
 audio/x-ms-wax wax
 audio/x-ms-wma wma
-audio/x-pn-realaudio ra rm ram
+audio/x-pn-realaudio ra ram rm
 audio/x-scpls pls
 audio/x-sd2 sd2
 audio/x-wav wav
@@ -57,24 +57,24 @@ font/woff woff
 font/woff2 woff2
 image/gif gif
 image/jp2 jp2 jpg2
-image/jpeg jpeg jpg jpe
-image/jpx jpx jpf
+image/jpeg jpe jpeg jpg
+image/jpx jpf jpx
 image/png png
 image/svg+xml svg svgz
-image/tiff tiff tif
+image/tiff tif tiff
 image/webp webp
 image/x-icon ico
 message/rfc822 eml mime
 text/css css
 text/csv csv
-text/html html htm shtml
-text/markdown md markdown
+text/html htm html shtml
+text/markdown markdown md
 text/mathml mml
-text/plain asc txt text pot log
+text/plain asc log pot text txt
 text/richtext rtx
 text/tab-separated-values tsv
-text/troff t tr roff man me ms
-text/x-c c cc cxx cpp c++ h hh hxx hpp h++
+text/troff man me ms roff t tr
+text/x-c c c++ cc cpp cxx h h++ hh hpp hxx
 text/x-diff diff patch
 video/mp4 mp4
 video/ogg ogv
