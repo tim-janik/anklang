ca54b0b66bc2d81ae9f81d672e12139c139bde4a
Tim Janik <timj@gnu.org>
2021-09-07 04:41:40 +0200

    MISC: version.sh: simplify version detection
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/version.sh
+++ misc/version.sh
@@ -2,6 +2,8 @@
 # This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 set -Eeuo pipefail
 
+SCRIPTNAME=${0##*/} ; die() { [ -z "$*" ] || echo "$SCRIPTNAME: $*" >&2; exit 9 ; }
+
 # Usage: version.sh [--last]		# print project version
 while test $# -ne 0 ; do
   case "$1" in
@@ -10,26 +12,23 @@ while test $# -ne 0 ; do
   esac
   shift
 done
-UNOFFICIAL=snapshot
 
 # Notes on scenario variants.
-# 1) The project tree may be created as a checkout with temporary GIT_DIR. Then, the only way
-#    to guess a version number is peeking at the top NEWS.md entry. This is never a release build.
-# 2) The project tree may be the result of extracting a 'git archive', in which case $Format...$
-#    extraction has occoured. The extracted refs may or may not contain a release tag. A tag
-#    will be present if the archive was a release tarball, or downloaded from release-tag
-#    version as Github archive. A release tag matching the top NEWS.md entry is a release build.
-# 3) We are operating on a normal Git repository, the current checkout may be referenced
-#    by a release tag. A release tag matching the top NEWS.md entry is a release build.
-
-# Output for release versions may look like:
-#    1.2.3 v1.2.3-tarball 2001-01-01 01:01:01 +0000
-# And for non-releases:
-#    1.2.3-alpha-3-ga1b2c3d4-dirty v1.2.3-alpha-3-ga1b2c3d4-dirty 2001-01-01 00:00:00 +0000
-# The first term is the version number and always starts with [0-9][0-9.]+.
+# 1) git-archive snapshots, version info is embedded via $Format$ strings
+# 2) Full git repo, use git describe
+# 3) Shallow git repo, use NEWS.md version info
+#
+# Version output may look like:
+#    1.2.3-rc1  v1.2.3-rc1-gita1b2c3d4          2001-01-01 00:00:00 +0000		# release
+#    1.2.3-rc1  v1.2.3-rc1-789-ga1b2c3d4-dirty  2001-01-01 00:00:00 +0000		# unreleased (with .git/)
+#    1.2.3-rc1  v1.2.3-rc1-unreleased-ga1b2c3d4 2001-01-01 00:00:00 +0000		# unreleased (via git-archive)
+# The first term is the version number and always starts with [0-9][0-9.]+,
+# it may be used to construct package names.
 # The second term is the build ID and may actually contain any kind of string.
 # The third term is the last commit date in ISO 8601-like format.
-exit_with_version() { # exit_with_version <version> <buildid> <releasedate>
+
+# exit_with_version <version> <buildid> <releasedate>
+exit_with_version() {
   V="${1#v}"	# strip 'v' prefix if any
   shift
   echo "$V" "$@"
@@ -38,54 +37,51 @@ exit_with_version() { # exit_with_version <version> <buildid> <releasedate>
 
 # Determine version from NEWS.md file
 peek_news_version() {
-  test -r NEWS.md || {
-    echo "$0: ERROR: Failed to find NEWS.md" >&2
-    exit 96
-  }
-  sed -nr '/^##\s.*[0-9]+\.[0-9]+\.[0-9]/ { s/.*\s([0-9]+\.[0-9]+\.[0-9]+[a-z0-9A-Z-]+)\b.*/\1/; p; q }' NEWS.md
+  test -r NEWS.md || die "ERROR: failed to find NEWS.md"
+  sed -nr '/^##\s.*[0-9]+\.[0-9]+\.[0-9]/ { s/.*\s([0-9]+\.[0-9]+\.[0-9]+[a-z0-9A-Z-]*)\b.*/\1/; p; q }' NEWS.md
 }
 
-# Determine version from ./.git if present
+# Yield abbreviated commit hash for version string
+shorthash() {
+  git log -1 --format='%h' --abbrev=13
+}
+
+# Tarball or git-archive: Find 'tag:...' in export-subst format string, see gitattributes(5)
+GITARCHIVE_HASH='$Format:%H$' && GITARCHIVE_DATE='$Format:%ci$' && GITARCHIVE_REFS='$Format:%D,$'
+if test "$GITARCHIVE_HASH" = "${GITARCHIVE_HASH/:/}" ; then
+  GITARCHIVE_TAG=$(echo " $GITARCHIVE_REFS" | tr , '\n' |
+		     sed -nr '/\btag: v?[0-9]\.[0-9]+\.[0-9]+/ { s/^[^0-9]*\b(v?[0-9]+\.[0-9]+\.[0-9]+[a-z0-9A-Z-]*).*/\1/; p; q }')
+  NEWS_VERSION="$(peek_news_version)"
+  if test -n "$GITARCHIVE_TAG" -a "${GITARCHIVE_TAG#v}" = "$NEWS_VERSION" ; then
+    # release version (from tarball)
+    exit_with_version "$NEWS_VERSION" "v$NEWS_VERSION-git${GITARCHIVE_HASH:0:13}" "$GITARCHIVE_DATE"
+  else
+    # not a release (git-archive)
+    exit_with_version "$NEWS_VERSION" "v$NEWS_VERSION-unreleased-g${GITARCHIVE_HASH:0:13}" "$GITARCHIVE_DATE"
+  fi
+fi
+
+# Git repository with history
 if COMMIT_DATE=$(git log -1 --format='%ci' 2>/dev/null) &&
     LAST_TAG=$(git describe --match v'[0-9]*.[0-9]*.*[0-9a]' --abbrev=0 --first-parent 2>/dev/null) &&
-    BUILD_ID=$(git describe --match "$LAST_TAG" --abbrev=8 --long --dirty) &&
-    test -n "$BUILD_ID"
+    BUILD_ID=$(git describe --match "$LAST_TAG" --abbrev=8 --long --dirty)
 then
-  if [[ "$BUILD_ID" =~ ^v?[0-9]+\..*-0-g[a-z0-9]+$ ]] ; then
-    NEWS_VERSION="$(peek_news_version)"
-    if test "${LAST_TAG#v}" = "$NEWS_VERSION" ; then
-      exit_with_version "$LAST_TAG" "$BUILD_ID" "$COMMIT_DATE"	# release version
-    fi
-  fi
-  # not a release
+  [[ "$BUILD_ID" =~ ^v?[0-9]+\..*-0-g[a-z0-9]+$ ]] &&
+    NEWS_VERSION="$(peek_news_version)" &&
+    test "${LAST_TAG#v}" = "$NEWS_VERSION" && {
+      # release version
+      exit_with_version "$NEWS_VERSION" "v$NEWS_VERSION-git`shorthash`" "$COMMIT_DATE"
+    }
+  # not a release (frequent case during development)
   exit_with_version "$LAST_TAG" "$BUILD_ID" "$COMMIT_DATE"
 fi
 
-# Find 'tag:...' in 'git archive' substitution (see man gitattributes / export-subst)
-GITARCHIVE_REFS='$Format:%D,$'
-GITARCHIVE_DATE='$Format:%ci$'
-[[ "$GITARCHIVE_REFS$GITARCHIVE_DATE" =~ '$'Format:% ]] && LAST_TAG= ||
-    LAST_TAG=$(echo " $GITARCHIVE_REFS" | tr , '\n' | \
-		 sed -nr '/\btag: v?[0-9]\.[0-9]+\.[0-9]+/ { s/^[^0-9]*\b(v?[0-9]+\.[0-9]+\.[0-9]+[a-z0-9A-Z-]*).*/\1/; p; q }')
-test -n "$LAST_TAG" && {
+# Shallow git repo, resorts to NEWS.md
+test -n "$COMMIT_DATE" && {
   NEWS_VERSION="$(peek_news_version)"
-  if test "${LAST_TAG#v}" = "$NEWS_VERSION" ; then
-    exit_with_version "$LAST_TAG" "$LAST_TAG-tarball" "$GITARCHIVE_DATE" # release version
-  else
-    BUILD_ID="$LAST_TAG-$UNOFFICIAL"
-    exit_with_version "$BUILD_ID" "$BUILD_ID-archive" "$GITARCHIVE_DATE"
-  fi
+  BUILD_ID="v$NEWS_VERSION-unreleased-g`shorthash`"
+  # not a release (shallow git repo)
+  exit_with_version "$NEWS_VERSION" "$BUILD_ID" "$COMMIT_DATE"
 }
 
-# Use NEWS_VERSION as last resort
-if NEWS_VERSION="$(peek_news_version)" ; then
-  test -n "$COMMIT_DATE" &&
-    NEWS_DATE="$COMMIT_DATE" ||
-      NEWS_DATE="$(stat -c %y NEWS.md | sed 's/\.[0-9]\+//')" ||
-      NEWS_VERSION=
-fi
-test -n "$NEWS_VERSION" &&
-  exit_with_version "$NEWS_VERSION-$UNOFFICIAL" "$NEWS_VERSION-$UNOFFICIAL$(date +-%y%m%d)" "$NEWS_DATE"
-
-echo "$0: ERROR: Failed to find project version, use a valid git repository or release tarball" >&2
-exit 97
+die "ERROR: failed to find git commit"
