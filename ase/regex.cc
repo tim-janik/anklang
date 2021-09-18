e1dd4117f5343237e7e4f8591e8405c5241f91b3
Tim Janik <timj@gnu.org>
2021-09-18 23:56:43 +0200

    ASE: regex: support Re::sub() with count argument
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ase/regex.cc
+++ ase/regex.cc
@@ -1,5 +1,6 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 #include "regex.hh"
+#include "internal.hh"
 #include <regex>
 
 namespace Ase {
@@ -10,7 +11,7 @@ regex_flags (Re::Flags flags, bool d = false)
   const bool ere = (flags & Re::ERE) == Re::ERE;
   auto o = ere ? std::regex::extended : std::regex::ECMAScript;
   if (!d)
-    o |= std::regex::nosubs;
+    o |= std::regex::nosubs;    // () groups are always (?:)
   if (Re::I & flags)    o |= std::regex::icase;
   // if (Re::M & flags) o |= std::regex::multiline;
   // if (Re::S & flags) o |= std::regex::dotall;
@@ -30,10 +31,51 @@ Re::search (const String &regex, const String &input, Flags flags)
 
 /// Substitute `regex` in `input` with `subst`.
 String
-Re::sub (const String &regex, const String &subst, const String &input, Flags flags)
+Re::sub (const String &regex, const String &subst, const String &input, uint count, Flags flags)
 {
+  const std::sregex_iterator end = std::sregex_iterator();
   std::regex rex (regex, regex_flags (flags));
-  return std::regex_replace (input, rex, subst);
+  std::sregex_iterator matchiter = std::sregex_iterator (input.begin(), input.end(), rex);
+  const size_t n = std::distance (matchiter, end); // number of matches
+  return_unless (n, input);
+  std::string result;
+  auto out = std::back_inserter (result);
+  std::sub_match<std::string::const_iterator> tail;
+  for (std::sregex_iterator it = matchiter; it != end; it++)
+    {
+      // std::smatch match = *it;
+      std::sub_match<std::string::const_iterator> prefix = it->prefix();
+      out = std::copy (prefix.first, prefix.second, out);
+      out = std::copy (subst.begin(), subst.end(), out);
+      tail = it->suffix();
+      if (count-- == 1)
+        break;
+    }
+  out = std::copy (tail.first, tail.second, out);
+  return result;
 }
 
 } // Ase
+
+#include "testing.hh"
+
+namespace { // Anon
+using namespace Ase;
+
+TEST_INTEGRITY (regex_tests);
+static void
+regex_tests()
+{
+  ssize_t k;
+  k = Re::search ("fail", "abc abc");                                   TCMP (k, ==, -1);
+  k = Re::search (R"(\bb)", "abc bbc");                                 TCMP (k, ==, 4);
+  String u, v;
+  u = "abc abc abc Abc"; v = Re::sub ("xyz", "ABC", u);                 TCMP (v, ==, "abc abc abc Abc");
+  u = "abc abc abc Abc"; v = Re::sub ("abc", "ABC", u);                 TCMP (v, ==, "ABC ABC ABC Abc");
+  u = "abc abc abc Abc"; v = Re::sub ("abc", "ABC", u, 2);              TCMP (v, ==, "ABC ABC abc Abc");
+  u = "abc abc abc Abc"; v = Re::sub ("abc", "ABC", u, 0, Re::I);       TCMP (v, ==, "ABC ABC ABC ABC");
+  u = "abc abc abc Abc"; v = Re::sub (R"(\bA)", "-", u);                TCMP (v, ==, "abc abc abc -bc");
+  u = "abc abc abc Abc"; v = Re::sub (R"(\bA\b)", "-", u);              TCMP (v, ==, "abc abc abc Abc");
+}
+
+} // Anon
