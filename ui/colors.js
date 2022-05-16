f43d033e44134fef8102768210537d1f3b03ea7a
Tim Janik <timj@gnu.org>
2022-05-17 01:44:57 +0200

    UI: colors.js: new file for color modifications
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ ui/colors.js
@@ -0,0 +1,7 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+'use strict';
+
+export const color_names = {
+};
+
+export const color_values = Array.from (new Set (Object.values (color_names)).values());
