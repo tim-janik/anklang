f1739cbf1a5f6d653a3826453c684f964ec25e10
Tim Janik <timj@gnu.org>
2022-05-20 01:04:26 +0200

    UI: little.js: use ui/postcss.js to process CSS in the browser
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/little.js
+++ ui/little.js
@@ -9,20 +9,11 @@ export const docs = (...args) => undefined;
 
 // == PostCSS ==
 const postcssjs = require ('postcss');		// browserified
-import postcss_config from './postcss.esm.js';	// require()s browserified plugins
-const postcss_processor = postcssjs (postcss_config.plugins);
+import * as PostCss from './postcss.js';	// require()s browserified plugins
 
 /// Process CSS via PostCSS, uses async plugins.
 export async function postcss_process (css_string, fromname = '<style string>') {
-  const options = Object.assign ({ from: fromname }, postcss_config);
-  let result;
-  try {
-    result = (await postcss_processor.process (css_string, options)).css;
-  } catch (ex) {
-    console.warn ('PostCSS input:', fromname + '\n', css_string);
-    console.error ('PostCSS error:', ex);
-    result = '';
-  }
+  const result = await PostCss.postcss_process (css_string, fromname);
   if (__DEV__) {
     const errs = require ('csstree-validator').validate (result, "input.postcss");
     if (errs.length) {
@@ -45,7 +36,6 @@ export async function postcss (...args) {
 async function memorize_imports (imports) {
   const fetchoptions = {};
   const fetchlist = imports.map (async filename =>
-    postcss_config.add_import (filename, await (await fetch (filename, fetchoptions)).text()));
+    PostCss.add_import (filename, await (await fetch (filename, fetchoptions)).text()));
   await Promise.all (fetchlist);
 }
-
