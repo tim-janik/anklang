a2021e686055b2536714493bd38aa6a62e3d3fd2
Tim Janik <timj@gnu.org>
2022-05-20 01:17:31 +0200

    UI: little.js: conditionally load and use csstree-validator.esm.js
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/little.js
+++ ui/little.js
@@ -8,16 +8,17 @@ import { LitElement, html, css, unsafeCSS } from '../lit.js';
 export const docs = (...args) => undefined;
 
 // == PostCSS ==
-const postcssjs = require ('postcss');		// browserified
 import * as PostCss from './postcss.js';	// require()s browserified plugins
+const csstree_validator = __DEV__ && await import ('./csstree-validator.esm.js');
 
 /// Process CSS via PostCSS, uses async plugins.
-export async function postcss_process (css_string, fromname = '<style string>') {
+export async function postcss_process (css_string, fromname = '<style string>', validate = true) {
   const result = await PostCss.postcss_process (css_string, fromname);
-  if (__DEV__) {
-    const errs = require ('csstree-validator').validate (result, "input.postcss");
+  if (__DEV__ && validate) {
+    const errs = csstree_validator.validate (result, "input.postcss");
     if (errs.length) {
-      console.error ('postcss``:' + errs[0].line + ': ' + errs[0].name + ': ' + errs[0].message + ': ' + errs[0].css + '\n', errs);
+      console.warn ('PostCSS output:', fromname + ':\n', css_string);
+      console.error (fromname + ':' + errs[0].line + ': ' + errs[0].name + ': ' + errs[0].message + ': ' + errs[0].css + '\n', errs);
       console.info (result);
     }
   }
