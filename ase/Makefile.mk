0fc6499a86eb748966fac858fe03dc8b7d8d47de
Tim Janik <timj@gnu.org>
2022-06-18 03:08:03 +0200

    ASE: Makefile.mk: build gtk2wrap.so
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ase/Makefile.mk
+++ ase/Makefile.mk
@@ -5,14 +5,14 @@ CLEANDIRS     += $(wildcard $>/ase/ $>/lib/)
 include ase/tests/Makefile.mk
 
 # == ase/ *.cc file sets ==
-ase/AnklangSynthEngine.main.cc	::= ase/main.cc
-ase/nonlib.cc			::= $(ase/AnklangSynthEngine.main.cc)
-ase/libsources.cc		::= $(filter-out $(ase/nonlib.cc), $(wildcard ase/*.cc))
+ase/gtk2wrap.sources		::= ase/gtk2wrap.cc
+ase/noglob.cc			::= ase/main.cc $(ase/gtk2wrap.sources)
+ase/libsources.cc		::= $(filter-out $(ase/noglob.cc), $(wildcard ase/*.cc))
 ase/libsources.c		::= $(wildcard ase/*.c)
 
 # == AnklangSynthEngine definitions ==
 lib/AnklangSynthEngine		::= $>/lib/AnklangSynthEngine-$(version_m.m.m)
-ase/AnklangSynthEngine.sources	::= $(ase/AnklangSynthEngine.main.cc) $(ase/libsources.cc) $(ase/libsources.c)
+ase/AnklangSynthEngine.sources	::= ase/main.cc $(ase/libsources.cc) $(ase/libsources.c)
 ase/AnklangSynthEngine.gensrc	::= $>/ase/api.jsonipc.cc
 ase/AnklangSynthEngine.deps	::= $>/ase/sysconfig.h
 ase/AnklangSynthEngine.deps	 += $>/external/websocketpp/server.hpp $>/external/rapidjson/rapidjson.h
@@ -122,6 +122,19 @@ $(call INSTALL_BIN_RULE, $(basename $(lib/AnklangSynthEngine)), $(DESTDIR)$(pkgd
   ))
 # silence some websocketpp warnings
 $(ase/AnklangSynthEngine.objects): EXTRA_CXXFLAGS ::= -Wno-sign-promo
+
+# == gtk2wrap.so ==
+lib/gtk2wrap.so         ::= $>/lib/gtk2wrap.so
+ase/gtk2wrap.objects    ::= $(call BUILDDIR_O, $(ase/gtk2wrap.sources))
+$(ase/gtk2wrap.objects): EXTRA_INCLUDES ::= -I$> $(GTK2_CFLAGS)
+$(ase/gtk2wrap.objects): EXTRA_CXXFLAGS ::= -Wno-deprecated -Wno-deprecated-declarations
+$(call BUILD_SHARED_LIB, \
+	$(lib/gtk2wrap.so), \
+	$(ase/gtk2wrap.objects), \
+	$(lib/libase.so) | $>/lib/, \
+	$(GTK2_LIBS), \
+	../lib)
+$(ALL_TARGETS) += $(lib/gtk2wrap.so)
 
 # == Check Integrity Tests ==
 check-ase-tests: $(lib/AnklangSynthEngine)
