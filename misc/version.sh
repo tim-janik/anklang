c1bab025a990e5e7003e40fcfe74d4bba74e1c28
Tim Janik <timj@gnu.org>
2022-06-21 15:25:08 +0200

    MISC: version.sh: support $VERSION_HASHLENGTH and --nightly
    Also, get rid of git archive release builds, it is unused.
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/version.sh
+++ misc/version.sh
@@ -3,7 +3,7 @@
 set -Eeuo pipefail
 
 SCRIPTNAME=${0##*/} ; die() { [ -z "$*" ] || echo "$SCRIPTNAME: $*" >&2; exit 9 ; }
-L=17
+L=${VERSION_HASHLENGTH:-7}
 
 # Notes on scenario variants.
 # 1) git-archive snapshots, version info is embedded via $Format$ strings
@@ -11,7 +11,7 @@ L=17
 # 3) Shallow git repo, use NEWS.md version info
 #
 # Version output may look like:
-#    1.2.3-rc4       v1.2.3-rc4-git4a3b2c1d          2001-01-01 00:00:00 +0000		# release
+#    1.2.3-rc4       v1.2.3-rc4-g4a3b2c1d            2001-01-01 00:00:00 +0000		# release
 #    1.2.3-rc4-dirty v1.2.3-rc4-789-ga1b2c3d4-dirty  2001-01-01 00:00:00 +0000		# unreleased (with .git/)
 #    1.2.3-rc4-dirty v1.2.3-rc4-ga1b2c3d4-dirty      2001-01-01 00:00:00 +0000		# unreleased (via git-archive)
 # The first term is the version number and always starts with [0-9][0-9.]+,
@@ -45,6 +45,15 @@ fetch_news_version() { # fetch_news_version {1|2}
   fi
 }
 
+# Make nightly version from git describe
+make_nightly() {
+  local V
+  V=$(git describe --first-parent --match v'[0-9]*.[0-9]*' --abbrev=$L --long Nightly) &&
+    V=$(echo "$V" | sed -r "s/^v//;    s/-.*-g/-nightly`date +%y%m%d`-g/")
+  [[ "$V" =~ [0-9.]+-nightly[0-9]+-g[a-f0-9]+$ ]] && true
+  echo "$V"
+}
+
 # Usage: version.sh [--news-tag|--last-tag]	# print project versions
 while test $# -ne 0 ; do
   case "$1" in
@@ -56,6 +65,8 @@ while test $# -ne 0 ; do
       NEWS_VERSION="$(fetch_news_version 2)"
       test -n "$NEWS_VERSION" || die "ERROR: failed to extract release tag from NEWS.md"
       echo "v${NEWS_VERSION#v}" && exit ;;
+    --nightly)
+      make_nightly && exit ;;
     *)
       true ;;
   esac
@@ -73,28 +84,25 @@ if test "$GITARCHIVE_HASH" = "${GITARCHIVE_HASH/:/}" ; then
   GITARCHIVE_TAG=$(echo " $GITARCHIVE_REFS" | tr , '\n' |
 		     sed -nr '/\btag: v?[0-9]+\.[0-9]+/ { s/^[^0-9]*\b(v?[0-9]+\.[0-9]+[.a-z0-9A-Z-]*).*/\1/; p; q }')
   NEWS_VERSION="$(fetch_news_version 1)"
-  if test -n "$GITARCHIVE_TAG" -a "${GITARCHIVE_TAG#v}" = "$NEWS_VERSION" ; then
-    # release version (from tarball)
-    exit_with_version "$NEWS_VERSION" "v$NEWS_VERSION-git${GITARCHIVE_HASH:0:$L}" "$GITARCHIVE_DATE"
-  else
-    # not a release (git-archive)
-    exit_with_version "$NEWS_VERSION-dirty" "v$NEWS_VERSION-g${GITARCHIVE_HASH:0:$L}-dirty" "$GITARCHIVE_DATE"
-  fi
+  # not a release (git-archive)
+  exit_with_version "$NEWS_VERSION-dirty" "v$NEWS_VERSION-g${GITARCHIVE_HASH:0:$L}-dirty" "$GITARCHIVE_DATE"
 fi
 
 # Git repository with history
 if COMMIT_DATE=$(git log -1 --format='%ci' 2>/dev/null) &&
     LAST_TAG=$(git describe --match v'[0-9]*.[0-9]*' --abbrev=0 --first-parent 2>/dev/null) &&
-    BUILD_ID=$(git describe --match "$LAST_TAG" --abbrev=$L --long --dirty)
+    BUILD_ID=$(git describe --match "$LAST_TAG" --abbrev=$L --long)
 then
-  [[ "$BUILD_ID" =~ ^v?[0-9]+\.[0-9].*-0-g[a-z0-9]+$ ]] &&
+  # Nightly releases
+  ${VERSION_SH_NIGHTLY:-false} &&
+    NIGHTLY_ID=$(make_nightly) &&
     NEWS_VERSION="$(fetch_news_version 1)" &&
-    test "${LAST_TAG#v}" = "$NEWS_VERSION" && {
-      # release version
-      exit_with_version "$NEWS_VERSION" "v$NEWS_VERSION-git`shorthash`" "$COMMIT_DATE"
+    test "${NIGHTLY_ID:-none}" = "$NEWS_VERSION" && {
+      # Nightly release version, ID already contains shorthash
+      exit_with_version "$NIGHTLY_ID" "v$NIGHTLY_ID" "$COMMIT_DATE"
     }
   # not a release (frequent case during development)
-  exit_with_version "$LAST_TAG-dirty" "${BUILD_ID%-dirty}-dirty" "$COMMIT_DATE"
+  exit_with_version "$LAST_TAG-dirty" "$BUILD_ID-dirty" "$COMMIT_DATE"
 fi
 
 # Shallow git repo, resorts to NEWS.md
