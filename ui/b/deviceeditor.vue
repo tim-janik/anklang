ddc454fe850e6d5ec474189165e2163c5468df4f
Tim Janik <timj@gnu.org>
2022-06-22 23:53:17 +0200

    UI: b/deviceeditor.vue: implement delete-device
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/b/deviceeditor.vue
+++ ui/b/deviceeditor.vue
@@ -37,9 +37,9 @@
 		   :name="group.name" :props="group.props" />
     </c-grid>
     <b-contextmenu ref="cmenu" @click="activation" :check="deactivation" >
-      <b-menutitle> Module </b-menutitle>
-      <b-menuitem fa="plus-circle"      uri="add-module" >      Add Module		</b-menuitem>
-      <b-menuitem fa="times-circle"     uri="delete-module" >   Delete Module		</b-menuitem>
+      <b-menutitle> Device </b-menutitle>
+      <b-menuitem fa="plus-circle"      uri="add-device" >      Add Device		</b-menuitem>
+      <b-menuitem fa="times-circle"     uri="delete-device" >   Delete Device		</b-menuitem>
     </b-contextmenu>
   </h-flex>
 </template>
@@ -256,13 +256,20 @@ export default {
     activation (uri) {
       // close popup to remove focus guards
       this.$refs.cmenu.close();
+      switch (uri) {
+	case 'delete-device':
+	  this.device.remove_self();
+	  break;
+      }
     },
     deactivation (uri, component) {
       if (!this.device)
 	return false;
       switch (uri) {
-	case 'add-module':
+	case 'add-device':
 	  return false;
+	case 'delete-device':
+	  return true;
       }
       return false;
     },
