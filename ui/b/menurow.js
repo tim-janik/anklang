b79018b392a5e2a866c5a602cd86b9bc88e88d8f
Tim Janik <timj@gnu.org>
2022-10-16 02:38:51 +0200

    UI: b/menurow.js: port b-menurow to LitElement
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ ui/b/menurow.js
@@ -0,0 +1,45 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+
+/** # B-MENUROW
+ * Menuitems are packed horizontally inside a menurow.
+ * ## Props:
+ * *noturn*
+ * : Avoid turning the icon-label direction in menu items to be upside down.
+ * ## Slots:
+ * *default*
+ * : All contents passed into this slot will be rendered as contents of this element.
+ */
+
+import { LitElement, html, css, postcss, docs } from '../little.js';
+
+// == STYLE ==
+const STYLE = await postcss`
+@import 'shadow.scss';
+:host {
+  display: inline-flex; flex: 0 0 auto; flex-wrap: nowrap; flex-direction: row;
+  text-align: center; justify-content: center; align-items: baseline;
+  margin: 0; padding: 0;
+}`;
+
+// == HTML ==
+const HTML = () => html`
+  <slot />
+`;
+
+// == SCRIPT ==
+const BOOL_ATTRIBUTE = { type: Boolean, reflect: true }; // sync attribute with property
+
+class BMenuRow extends LitElement {
+  static styles = [ STYLE ];
+  render = HTML;
+  static properties = {
+    noturn: BOOL_ATTRIBUTE,
+  };
+  constructor()
+  {
+    super();
+    this.noturn = false;
+  }
+}
+
+customElements.define ('b-menurow', BMenuRow);
