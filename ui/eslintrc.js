8f51c57ede20459a2d10b2de7d79702c667e9f21
Tim Janik <timj@gnu.org>
2022-12-17 12:54:05 +0100

    UI: eslintrc.js: allow empty blocks
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/eslintrc.js
+++ ui/eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
 
   rules: {
     "no-restricted-globals": ["warn", "event", /*"error"*/ ],
+    "no-empty": [ 'warn' ],
     "no-loss-of-precision": OFF,
     "no-unused-vars": OFF, // see unused-imports/no-unused-vars
     "unused-imports/no-unused-vars": [ "warn", { args: "none", varsIgnorePattern: "^_.*" } ],
