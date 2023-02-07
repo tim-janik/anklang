9bb10d1d2748653d47eb739a1e7c818bf949a53a
Tim Janik <timj@gnu.org>
2023-02-07 17:03:46 +0100

    UI: wrapper.js: implement define_reactive() and reactive_wrapper()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/wrapper.js
+++ ui/wrapper.js
@@ -110,7 +110,7 @@ export function wrap_ase_object (aseobj, fields = {}, callback = undefined)
   let cwrapper = aseobj_weakmap.get (aseobj);
   // make facade with aseobj prototype for automatic cleanup
   const __cleanup__ = () => {
-    facade_cleanup_registry.unregister (__cleanup__);
+    finalization_cleanup_registry.unregister (__cleanup__);
     while (__cleanup__.cleanups.length)
       __cleanup__.cleanups.shift() ();
     cwrapper = null;
@@ -129,10 +129,123 @@ export function wrap_ase_object (aseobj, fields = {}, callback = undefined)
 		   __add__,
 		   get __promise__() { return cwrapper?.__promise__; },
   };
-  facade_cleanup_registry.register (facade, __cleanup__, __cleanup__);
+  finalization_cleanup_registry.register (facade, __cleanup__, __cleanup__);
   // add property caching
   for (const prop of Object.getOwnPropertyNames (fields))
     facade.__add__ (prop, fields[prop], callback);
   return facade;
 }
-const facade_cleanup_registry = new FinalizationRegistry (cleanupfunc => cleanupfunc());
+
+/// FinalizationRegistry to call cleanup callback upon object destruction.
+const finalization_cleanup_registry = new FinalizationRegistry (cleanupfunc => cleanupfunc());
+
+/// Define reactive properties on `object`, to be used with reactive_wrapper().
+/// See also [Object.defineProperties](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties).
+export function define_reactive (object, properties_object)
+{
+  const data = {};
+  for (const key of Object.keys (properties_object))
+    {
+      const descriptor = properties_object[key] instanceof Object ? properties_object[key] : {};
+      const enumerable = descriptor.enumerable !== undefined ? !!descriptor.enumerable : true;
+      const configurable = descriptor.configurable !== undefined ? !!descriptor.configurable : false;
+      const writable = descriptor.writable !== undefined ? !!descriptor.writable : true;
+      const value = 'value' in descriptor ? descriptor.value : properties_object[key];
+      if (!writable) {
+	Object.defineProperty (object, key, { enumerable, configurable, value });
+	continue;
+      }
+      data[key] = value;
+      let notify_set = undefined;
+      Object.defineProperty (object, key, {
+	enumerable,
+	get () {
+	  if (reactive_tracking())
+	    reactive_track (notify_set || (notify_set = new Set));
+	  return data[key];
+	},
+	set (v) {
+	  if (v === data[key]) return;
+	  data[key] = v;
+	  reactive_notify (notify_set);
+	},
+	configurable });
+    }
+}
+
+function reactive_tracking()
+{
+  return !!reactive_effect_handle;
+}
+let reactive_effect_handle = null;
+
+function reactive_track (notify_set)
+{
+  const handle = reactive_effect_handle && reactive_effect_handle.deref();
+  handle?.track (notify_set);
+}
+
+function reactive_notify (notify_set)
+{
+  if (!notify_set) return;
+  const call_handle_notify = handle_wref => {
+    const handle = handle_wref.deref();
+    if (handle)
+      handle.notify();
+    else // stale WeakRef
+      notify_set.delete (handle_wref);
+  };
+  notify_set.forEach (call_handle_notify);
+}
+
+// Internal handle for reactive_wrapper()
+class ReactiveHandle {
+  wref = null;
+  notifier = null;
+  notify_sets = null;
+  effect = null;
+  once = false;
+  running = 0;
+  notify ()
+  {
+    if (this.once)
+      this.untrack();
+    this.notifier();
+  }
+  run ()
+  {
+    if (!this.running) // recursion
+      this.untrack();
+    const last_handle = reactive_effect_handle;
+    reactive_effect_handle = this.wref;
+    this.running += 1;
+    const result = this.effect (...arguments);
+    this.running -= 1;
+    reactive_effect_handle = last_handle;
+    return result;
+  }
+  untrack ()
+  {
+    if (!this.notify_sets) return;
+    this.notify_sets.forEach (nset => nset.delete (this.wref));
+    this.notify_sets.clear();
+  }
+  track (nset)
+  {
+    this.notify_sets || (this.notify_sets = new Set);
+    this.notify_sets.add (nset);
+    nset.add (this.wref);
+  }
+}
+
+/// Make `effect()` wrapper to watch reactive properties, on changes run `notifier()`.
+export function reactive_wrapper (effect, notifier, keepwatching = false)
+{
+  const handle = new ReactiveHandle;
+  handle.wref = new WeakRef (handle);
+  handle.notifier = notifier;
+  handle.effect = effect;
+  handle.once = !keepwatching;
+  handle.run = handle.run.bind (handle);
+  return handle.run;
+}
