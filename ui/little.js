2d279bff1c08c8110c313bbc685d7f128cbd9cc8
Tim Janik <timj@gnu.org>
2023-03-10 02:24:06 +0100

    UI: little.js: add default define $themename_scss:dark.scss;
    Also, disable csstree-validator which yields outdated warnings.
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/little.js
+++ ui/little.js
@@ -53,12 +53,16 @@ import * as PostCss from './postcss.js';	// require()s browserified plugins
 const csstree_validator = __DEV__ && await import ('./csstree-validator.esm.js');
 
 /// Process CSS via PostCSS, uses async plugins.
-export async function postcss_process (css_string, fromname = '<style string>', validate = true) {
-  const result = await PostCss.postcss_process (css_string, fromname, { import_all: true });
+export async function postcss_process (css_string, fromname = '<style string>', validate = false) {
+  const options = {
+    import_all: true,
+    vars: { themename_scss: 'dark.scss' },
+  };
+  const result = await PostCss.postcss_process (css_string, fromname, options);
   if (__DEV__ && validate) {
     const errs = csstree_validator.validate (result, "input.postcss");
     if (errs.length) {
-      console.warn ('PostCSS output:', fromname + ':\n', css_string);
+      // console.warn ('PostCSS output:', fromname + ':\n', css_string);
       console.error (fromname + ':' + errs[0].line + ': ' + errs[0].name + ': ' + errs[0].message + ': ' + errs[0].css + '\n', errs);
       console.info (result);
     }
