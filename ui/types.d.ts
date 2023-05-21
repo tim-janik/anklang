1c10d4697559cc84263cd670f7654d5919d08526
Tim Janik <timj@gnu.org>
2023-05-21 05:02:29 +0200

    UI: types.d.ts: allow App, Data, Shell Ase globals
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ ui/types.d.ts
@@ -0,0 +1,14 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+
+// Allow JavaScript modules as Any
+// declare module "*";
+
+// Allow certain globals
+declare global {
+  var App: any;
+  var Ase: any;
+  var Data: any;
+  var Shell: any;
+}
+
+export {};
