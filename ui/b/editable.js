e8864225fd7757bcbd34668ea1274b24814bbaf1
Tim Janik <timj@gnu.org>
2023-07-02 01:49:47 +0200

    UI: b/editable.js: cleanup CSS
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/b/editable.js
+++ ui/b/editable.js
@@ -24,7 +24,7 @@ JsExtract.scss`
 input {
   position: absolute;
   display: inline-block;
-  top: 0; left: 0; right: 0; bottom: 0;
+  inset: 0;
   width: 100%; height: 100%;
   margin: 0; padding: 0; border: 0;
   width: 100%; height: 100%; line-height: 1;
