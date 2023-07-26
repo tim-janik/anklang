9782c8ce2529ebe53668513c63c36092f2601edb
Tim Janik <timj@gnu.org>
2023-07-26 03:39:25 +0200

    UI: b/more.js: add CSS to globals.css, avoid shadowRoot
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/b/more.js
+++ ui/b/more.js
@@ -8,30 +8,27 @@ import { LitComponent, html, JsExtract, docs } from '../little.js';
  */
 
 // == STYLE ==
-const STYLE = await JsExtract.fetch_css (import.meta);
 JsExtract.scss`
-@import 'mixins.scss';
-:host { // b-more
+b-more { // b-more
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-}
-.-plus {
-  display: flex;
-  height: 90%;
-  align-items: center;
-  justify-content: space-evenly;
-  padding: 0 0.2em;
-  margin: 0 0.2em;
-  border-radius: 3px;
-  @include b-font-weight-bold();
-  font-size: 120%;
-  cursor: default; user-select: none;
-  border: 1px solid #bbb3;
-  color: #bbb;
-  &:hover { border: 1px solid #bbba; }
-}
-`;
+  .-plus {
+    display: flex;
+    height: 90%;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 0 0.2em;
+    margin: 0 0.2em;
+    border-radius: 3px;
+    @include b-font-weight-bold();
+    font-size: 120%;
+    cursor: default; user-select: none;
+    border: 1px solid #bbb3;
+    color: #bbb;
+    &:hover { border: 1px solid #bbba; }
+  }
+}`;
 
 // == HTML ==
 const HTML = html`
@@ -42,7 +39,7 @@ const HTML = html`
 
 // == SCRIPT ==
 class BMore extends LitComponent {
+  createRenderRoot() { return this; }
   render = () => HTML;
-  static styles = [ STYLE ];
 }
 customElements.define ('b-more', BMore);
