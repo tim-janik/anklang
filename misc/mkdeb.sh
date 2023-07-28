10d8bae4bda213c2841ff58e29dced7628b287dc
Tim Janik <timj@gnu.org>
2023-07-28 23:36:37 +0200

    MISC: mkdeb.sh: create debian package in assets/
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/mkdeb.sh
+++ misc/mkdeb.sh
@@ -144,6 +144,6 @@ find $DEBIAN/../ -name '*.la' -delete
 find $DEBIAN/../ -name '*.py[co]' -delete
 
 # create binary deb
-fakeroot dpkg-deb -Zzstd -z9 -b $DROOT $DROOT/..
-ls -al $BUILDDIR/$NAME''_$VERSION''_$ARCH.deb
-echo lintian -i --no-tag-display-limit $BUILDDIR/$NAME''_$VERSION''_$ARCH.deb
+fakeroot dpkg-deb -Zzstd -z9 -b $DROOT assets/
+ls -al assets/$NAME''_$VERSION''_$ARCH.deb
+# lintian -i --no-tag-display-limit assets/$NAME''_$VERSION''_$ARCH.deb
