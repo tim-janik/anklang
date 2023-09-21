d08912551e33e9347a70d7490119725cda42a577
Tim Janik <timj@gnu.org>
2023-09-21 03:32:42 +0200

    ASE: minizip: build against external/minizip-ng/
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ase/minizip.h
+++ ase/minizip.h
@@ -8,17 +8,17 @@
 #define MZ_ZIP_NO_ENCRYPTION
 
 // Minizip API
-#include "external/minizip/mz.h"
-#include "external/minizip/mz_os.h"
-#include "external/minizip/mz_zip.h"
-#include "external/minizip/mz_strm.h"
-//#include "external/minizip/mz_crypt.h"
-#include "external/minizip/mz_strm_buf.h"
-#include "external/minizip/mz_strm_mem.h"
-#include "external/minizip/mz_strm_zlib.h"
-#include "external/minizip/mz_strm_split.h"
-#include "external/minizip/mz_strm_os.h"
-#include "external/minizip/mz_zip_rw.h"
-//#include "external/minizip/mz_compat.h"
+#include "external/minizip-ng/mz.h"
+#include "external/minizip-ng/mz_os.h"
+#include "external/minizip-ng/mz_zip.h"
+#include "external/minizip-ng/mz_strm.h"
+//#include "external/minizip-ng/mz_crypt.h"
+#include "external/minizip-ng/mz_strm_buf.h"
+#include "external/minizip-ng/mz_strm_mem.h"
+#include "external/minizip-ng/mz_strm_zlib.h"
+#include "external/minizip-ng/mz_strm_split.h"
+#include "external/minizip-ng/mz_strm_os.h"
+#include "external/minizip-ng/mz_zip_rw.h"
+//#include "external/minizip-ng/mz_compat.h"
 
 #endif // __ASE_MINIZIP_H__
