e8ecf0c0ee7a83bdb8fc5193ca73f7a8b8fb2e70
Tim Janik <timj@gnu.org>
2023-10-14 21:57:39 +0200

    ASE: defs.hh: add PropertyImpl
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ase/defs.hh
+++ ase/defs.hh
@@ -60,6 +60,7 @@ ASE_CLASS_DECLS (Preference);
 ASE_CLASS_DECLS (Project);
 ASE_CLASS_DECLS (ProjectImpl);
 ASE_CLASS_DECLS (Property);
+ASE_CLASS_DECLS (PropertyImpl);
 ASE_CLASS_DECLS (ResourceCrawler);
 ASE_CLASS_DECLS (Server);
 ASE_CLASS_DECLS (ServerImpl);
