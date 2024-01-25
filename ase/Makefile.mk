e70586945ccf9376a78c4f8aa36631bd68c85ef2
Stefan Westerfeld <stefan@space.twc.de>
2024-01-25 17:25:28 +0100

    ASE: Makefile.mk: reorder static libs, liblilv depends on the others
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- ase/Makefile.mk
+++ ase/Makefile.mk
@@ -147,10 +147,10 @@ ASE_EXTERNAL_INCLUDES := $(strip	\
 	-Iexternal/websocketpp		\
 )
 LILV_LIBS := $(strip 					\
+	$>/lib/liblilv-0.a	\
 	$>/lib/libserd-0.a	\
 	$>/lib/libsord-0.a	\
 	$>/lib/libsratom-0.a	\
-	$>/lib/liblilv-0.a	\
 	$>/lib/libzix-0.a	\
 )
 
