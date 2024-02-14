3faf1aa0d46739ccd8620d0d3e4cb52187f6bc08
Stefan Westerfeld <stefan@space.twc.de>
2024-02-14 11:45:34 +0100

    devices/freeverb/freeverb.cc: merge dry/wet parameters into mix parameter
    See #23. Also add parameter smoothing for mix to avoid clicks when
    changing/automating the parameter.
    
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- devices/freeverb/freeverb.cc
+++ devices/freeverb/freeverb.cc
@@ -1,6 +1,7 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 #include "ase/processor.hh"
 #include "ase/internal.hh"
+#include "devices/blepsynth/linearsmooth.hh"
 
 namespace {
 
@@ -15,6 +16,8 @@ class Freeverb : public AudioProcessor {
   IBusId stereoin;
   OBusId stereout;
   revmodel model;
+  LinearSmooth mix_smooth_;
+  bool mix_smooth_reset_ = false;
 public:
   Freeverb (const ProcessorSetup &psetup) :
     AudioProcessor (psetup)
@@ -28,7 +31,7 @@ public:
     info.website_url = "https://beast.testbit.eu";
     info.creator_name = "Jezar at Dreampoint";
   }
-  enum Params { MODE = 1, DRY, WET, ROOMSIZE, DAMPING, WIDTH };
+  enum Params { MODE = 1, MIX, ROOMSIZE, DAMPING, WIDTH };
   void
   initialize (SpeakerArrangement busses) override
   {
@@ -40,8 +43,7 @@ public:
     ParameterMap pmap;
 
     pmap.group = _("Reverb Settings");
-    pmap[DRY] = Param ("drylevel", _("Dry level"), _("Dry"), scaledry * initialdry, "dB", { 0, scaledry });
-    pmap[WET] = Param ("wetlevel", _("Wet level"), _("Wet"), scalewet * initialwet, "dB", { 0, scalewet });
+    pmap[MIX] = Param { "mix",   "Mix dry/wet", "Mix", 30, "%", { 0, 100 } };
 
     ChoiceS centries;
     centries += { "Signflip 2000",  _("Preserve May 2000 Freeverb damping sign flip") };
@@ -62,8 +64,9 @@ public:
   {
     switch (Params (paramid))
       {
-      case WET:         return model.setwet (get_param (paramid) / scalewet);
-      case DRY:         return model.setdry (get_param (paramid) / scaledry);
+      case MIX:         mix_smooth_.set (get_param (paramid) * 0.01, mix_smooth_reset_);
+                        mix_smooth_reset_ = false;
+                        break;
       case ROOMSIZE:    return model.setroomsize ((get_param (paramid) - offsetroom) / scaleroom);
       case WIDTH:       return model.setwidth (0.01 * get_param (paramid));
       case MODE:
@@ -75,7 +78,11 @@ public:
   reset (uint64 target_stamp) override
   {
     model.setmode (0);          // no-freeze, allow mute
+    model.setdry (0);           // no dry, we mix the output signal during render
+    model.setwet (1);           // only need reverb wet
     model.mute();               // silence internal buffers
+    mix_smooth_.reset (sample_rate(), 0.020);
+    mix_smooth_reset_ = true;
     adjust_all_params();
   }
   void
@@ -86,7 +93,14 @@ public:
     float *input1 = const_cast<float*> (ifloats (stereoin, 1));
     float *output0 = oblock (stereout, 0);
     float *output1 = oblock (stereout, 1);
+    // this only generates the wet signal
     model.processreplace (input0, input1, output0, output1, n_frames, 1);
+    for (uint i = 0; i < n_frames; i++)
+      {
+        float mix = mix_smooth_.get_next();
+        output0[i] = input0[i] * (1 - mix) + output0[i] * mix;
+        output1[i] = input1[i] * (1 - mix) + output1[i] * mix;
+      }
   }
 };
 static auto freeverb = register_audio_processor<Freeverb> ("Ase::Devices::Freeverb");
