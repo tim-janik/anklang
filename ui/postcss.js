ee67e87d7762aa98049b309a6aa19c2443b2b511
Tim Janik <timj@gnu.org>
2024-02-29 23:53:49 +0100

    ui/postcss.js: add "dim" color scheme, a smoothed zinc like color
    Signed-off-by: Tim Janik <timj@gnu.org>

--- ui/postcss.js
+++ ui/postcss.js
@@ -30,6 +30,23 @@ const tailwind_config = {
       ...theme ('colors'),
       DEFAULT: 'var(--tw-border-default-color)', // theme ('colors.gray.200', 'currentColor'),
     }),
+    extend: {
+      colors: {
+        dim: {
+	  50:  '#f9f9ff',
+	  100: '#f3f3fe',
+	  200: '#e3e3ef',
+	  300: '#c7c7d2',
+	  400: '#a0a0aa',
+	  500: '#76767f',
+	  600: '#515159',
+	  700: '#36363e',
+	  800: '#26262e',
+	  900: '#16161e',
+	  950: '#07060f',
+	},
+      },
+    },
   },
 };
 
