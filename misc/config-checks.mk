9974ffa8d8833bd060b6342e29b2c61fd69a76b6
Tim Janik <timj@gnu.org>
2025-02-06 01:28:42 +0100

    misc/config-checks.mk: provide $(RUNTS) to run typescript files
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/config-checks.mk
+++ misc/config-checks.mk
@@ -115,6 +115,9 @@ $>/config-cache.mk: misc/config-checks.mk misc/version.sh $(GITCOMMITDEPS) | $>/
 	  || { pnpm --version 2>&1 | grep -qE '^([89]|[1-9]+[0-9]+)\.[0-9]+\.[0-9]+$$' \
 	       && echo 'XNPM ::= pnpm'				>>$@.tmp ; } \
 	  || echo 'XNPM ::= npm'				>>$@.tmp
+	$Q bun --version 2>&1 | grep -qE '^[1-9]+[0-9]*\.[0-9]+' \
+	  && echo 'RUNTS := bun run'				>>$@.tmp \
+	  || echo 'RUNTS := node --experimental-strip-types'	>>$@.tmp
 	$Q GTK2_CFLAGS=$$($(PKG_CONFIG) --cflags $(GTK2_PACKAGES)) \
 	  && echo "GTK2_CFLAGS ::= $$GTK2_CFLAGS"		>>$@.tmp
 	$Q GTK2_LIBS=$$($(PKG_CONFIG) --libs $(GTK2_PACKAGES)) \
