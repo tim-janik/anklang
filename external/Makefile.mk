ba7c51d7be010f26880ca98d33dbc3b9ebd6c9b1
Tim Janik <timj@gnu.org>
2025-07-28 12:04:11 +0200

    external/Makefile.mk, .gitmodules: fetch blake3-1.8.2 tarball, remove submodule
    Signed-off-by: Tim Janik <timj@gnu.org>

--- external/Makefile.mk
+++ external/Makefile.mk
@@ -11,6 +11,17 @@ sha256sum -c <<<'$(strip $2)  $(strip $1)' || \
   { echo '$(strip $1): ERROR: failed to fetch: $(strip $3)' >&2 ; exit 1 ; }
 endef
 
+# == external/blake3/ ==
+blake3/sha := 6b51aefe515969785da02e87befafc7fdc7a065cd3458cf1141f29267749e81f
+external/blake3/.sha-$(blake3/sha):
+	$(QGEN)
+	$Q $(call fetch-and-check, external/blake3.tar.gz, $(blake3/sha), \
+		https://github.com/BLAKE3-team/BLAKE3/archive/1.8.2/develop.tar.gz)
+	$Q rm -rf external/blake3 && mkdir external/blake3
+	$Q tar xf external/blake3.tar.gz --strip-components=1 -C external/blake3/
+	$Q touch $@
+EXTERNAL_CXX_STAMPS += external/blake3/.sha-$(blake3/sha)
+
 # == external/minizip-ng/ ==
 minizip-ng/sha := 80d745e1c8caf6f81f6457403b0d9212e8a138b2badd6060e8a5da8583da2551
 external/minizip-ng/.sha-$(minizip-ng/sha):
