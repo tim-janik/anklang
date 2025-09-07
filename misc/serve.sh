b1b6a738cc130435014d2740bac692281efb7875
Tim Janik <timj@gnu.org>
2025-09-08 01:29:38 +0200

    misc/serve.sh: add env variables for development server ports
    Signed-off-by: Tim Janik <timj@gnu.org>

--- misc/serve.sh
+++ misc/serve.sh
@@ -2,8 +2,14 @@
 # This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 set -Eeuo pipefail
 
+test "${1-}" == -x && { shift; set -x; }
+
 # Dir to find Anklang, run mkdocs, used by ui/vite.config.ts
 export BUILDDIR=out/
+# Note1: For development, TCP ports are hard coded, keep in sync with vite.config.ts
+export DEVPORT_ANKLANG=1776
+export DEVPORT_VITE=1777
+export DEVPORT_MKDOCS=1778
 
 # Signals to relay to children or process group
 SIGNALS="HUP INT QUIT ILL TRAP ABRT BUS SEGV USR2 PIPE TERM SYS"
@@ -16,28 +22,26 @@ else					# Kill child processes upon exit
   trap "trap - TERM EXIT; kill -TERM -- \$(jobs -p)" $SIGNALS ERR EXIT
 fi
 
-# Note1: For development, TCP ports are hard coded, keep in sync with vite.config.ts
-
 # Note2: Anklang uses an authentication token, stored under $HOME to restrict WebUI access
 # to one user. For development, we could read the ~/.../anklang*.html redirect, fetch the
 # auth URL and copy the set-cookie reply into the vite server headers.
 # It is just pointless as it still leaves vite open, so we just use --no-auth instead.
 
 # Anklang
-echo -e "\n+ $BUILDDIR/lib/AnklangSynthEngine --unauth-dev=1776 $*" >&2
-$BUILDDIR/lib/AnklangSynthEngine --unauth-dev=1776 "$@" &	# does setpgid()
+echo -e "\n+ $BUILDDIR/lib/AnklangSynthEngine --unauth-dev=$DEVPORT_ANKLANG $*" >&2
+$BUILDDIR/lib/AnklangSynthEngine --unauth-dev=$DEVPORT_ANKLANG "$@" &	# does setpgid()
 sleep 0.5
 
 # Vite
-echo -e "\n+ node_modules/.bin/vite --strictPort --host localhost --port 1777 -c ui/vite.config.ts -l info" >&2
-node_modules/.bin/vite --strictPort --host localhost --port 1777 -c ui/vite.config.ts -l info &
+echo -e "\n+ node_modules/.bin/vite --strictPort --host localhost --port $DEVPORT_VITE -c ui/vite.config.ts -l info" >&2
+node_modules/.bin/vite --strictPort --host localhost --port $DEVPORT_VITE -c ui/vite.config.ts -l info &
 sleep 0.5
 
 # MkDocs
-pushd $BUILDDIR >/dev/null
-echo -e "\n+ uv run mkdocs serve -a localhost:1778" >&2
-uv run mkdocs serve -a localhost:1778 &
-popd >/dev/null
+cd $BUILDDIR/mkdocs/
+echo -e "\n+ uv run mkdocs serve -a localhost:$DEVPORT_MKDOCS" >&2
+uv run mkdocs serve -a localhost:$DEVPORT_MKDOCS &
+cd - 2>/dev/null
 #sleep 1
 
 # Wait until first child exits
