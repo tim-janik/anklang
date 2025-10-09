bbf30a68538f411f74efb680897bbf483b3b9644
Tim Janik <timj@gnu.org>
2025-10-09 03:01:46 +0200

    trkn/juce.hh: add header to provide JuceHeader.h includes
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ trkn/juce.hh
@@ -0,0 +1,22 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+#pragma once
+
+/* Header to be pre-compiled to provide all needed Juce includes */
+
+#include "trkn/config.hh"
+
+// JuceHeader.h
+#include "juce_audio_basics/juce_audio_basics.h"
+#include "juce_core/juce_core.h"
+#include "juce_audio_devices/juce_audio_devices.h"
+#include "juce_events/juce_events.h"
+#include "juce_audio_formats/juce_audio_formats.h"
+#include "juce_audio_processors/juce_audio_processors.h"
+#include "juce_gui_extra/juce_gui_extra.h"
+#include "juce_gui_basics/juce_gui_basics.h"
+#include "juce_graphics/juce_graphics.h"
+#include "juce_data_structures/juce_data_structures.h"
+#include "juce_audio_utils/juce_audio_utils.h"
+#include "juce_dsp/juce_dsp.h"
+#include "juce_osc/juce_osc.h"
+
