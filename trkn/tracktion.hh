acb56eab44f93ced82fe779c291f1e8b6b3cb234
Tim Janik <timj@gnu.org>
2025-10-09 03:01:46 +0200

    trkn/tracktion.hh: add header for all tracktion_engine includes
    Signed-off-by: Tim Janik <timj@gnu.org>

---
+++ trkn/tracktion.hh
@@ -0,0 +1,12 @@
+// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
+#pragma once
+
+/* Header to be pre-compiled to provide all needed tracktion_engine includes */
+
+#include "trkn/juce.hh"
+
+// trkn/
+#include "tracktion_engine/tracktion_engine.h"
+#include "tracktion_graph/tracktion_graph.h"
+#include "tracktion_core/tracktion_core.h"
+
