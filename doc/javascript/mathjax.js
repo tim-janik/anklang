0a2b009fa0484657e8ee44b61f0cf44d59748a6c
Tim Janik <timj@gnu.org>
2025-10-13 03:50:11 +0200

    doc/javascript/mathjax.js: the SVG renderer might not have clearCache()
    Signed-off-by: Tim Janik <timj@gnu.org>

--- doc/javascript/mathjax.js
+++ doc/javascript/mathjax.js
@@ -14,7 +14,7 @@ window.MathJax = {
 };
 
 document$.subscribe (() => {
-  MathJax.startup.output.clearCache()
+  MathJax.startup.output.clearCache?.();
   MathJax.typesetClear()
   MathJax.texReset()
   MathJax.typesetPromise()
