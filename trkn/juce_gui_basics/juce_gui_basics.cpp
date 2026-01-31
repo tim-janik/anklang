25adabf0f34d5eb44922ab1148e4be2b9ef3374f
Tim Janik <timj@gnu.org>
2026-01-31 01:21:00 +0100

    trkn/juce_gui_basics/juce_gui_basics.cpp: unique_ptr fix [C++23]
    * Avoid unique_ptr instantiation on incomplete type [C++23]
    * To make unique_ptr<AccessibilityNativeImpl> a complete type, move the
      juce_Accessibility.cpp include before juce_AccessibilityHandler.cpp.
    
    Signed-off-by: Tim Janik <timj@gnu.org>

--- trkn/juce_gui_basics/juce_gui_basics.cpp
+++ trkn/juce_gui_basics/juce_gui_basics.cpp
@@ -225,6 +225,7 @@
 #endif
 
 //==============================================================================
+#include "native/accessibility/juce_Accessibility.cpp"
 #include "accessibility/juce_AccessibilityHandler.cpp"
 #include "application/juce_Application.cpp"
 #include "buttons/juce_ArrowButton.cpp"
@@ -312,7 +313,6 @@
 #include "mouse/juce_MouseInactivityDetector.cpp"
 #include "mouse/juce_MouseInputSource.cpp"
 #include "mouse/juce_MouseListener.cpp"
-#include "native/accessibility/juce_Accessibility.cpp"
 #include "native/juce_ScopedDPIAwarenessDisabler.cpp"
 #include "positioning/juce_MarkerList.cpp"
 #include "positioning/juce_RelativeCoordinate.cpp"
