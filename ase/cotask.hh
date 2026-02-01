55b766d874408300021ab694410425b518b6f1a5
Tim Janik <timj@gnu.org>
2026-02-01 18:32:20 +0100

    ase/cotask.hh: add generic CoTask<ReturnValue> implementation

--- ase/cotask.hh
+++ ase/cotask.hh
@@ -32,4 +32,124 @@ struct DetachedTask {
   };
 };
 
+/// Helper for CoTask<>
+struct CoTaskAux {
+  template<typename promise_type>
+  struct FinalAwaiter {
+    // Always suspend, so CoTask<>::frame_ stays valid
+    constexpr bool await_ready() const noexcept { return false; }
+    /// Resume the parent frame that is co_await-ing this CoTask<>
+    std::coroutine_handle<>
+    await_suspend (std::coroutine_handle<promise_type> h)
+    {
+      auto &promise = h.promise();      // `h` is the CoTask<> handle, currently in co_return
+      if (promise.continuation_)        // we have an awaiting caller
+        return promise.continuation_;   // jump to awaiting caller
+      if (promise.exception_)           // excption without continuation
+        ase_rethrow (promise.exception_);    // must not be forgotten
+      return std::noop_coroutine();     // Detached root, suspend is a no-op
+    }
+    constexpr void await_resume() noexcept { ASE_ASSERT_UNREACHED(); }
+  };
+  struct promise_base  {
+    std::exception_ptr exception_ = nullptr;            // Exception thrown from this coroutine
+    void unhandled_exception() noexcept { exception_ = std::current_exception(); }
+    // Always create + return CoTask, before starting/resuming execution
+    constexpr std::suspend_always initial_suspend() noexcept { return {}; }
+  };
+};
+
+/** General purpose coroutine task.
+ * This class represents a unit of asynchronous work that lazily executes
+ * when awaited. It is designed for composition (Task A awaits Task B).
+ */
+template<typename Result>
+struct CoTask {
+  static_assert (!std::is_reference_v<Result>, "The Result in CoTask<Result> must be movable or copyable");
+  struct promise_type : CoTaskAux::promise_base {
+    std::coroutine_handle<> continuation_ = nullptr;    // Waiter frame co_await-ing this CoTask
+    CoTask get_return_object() noexcept { return CoTask { std::coroutine_handle<promise_type>::from_promise (*this) }; }
+    CoTaskAux::FinalAwaiter<promise_type> final_suspend() noexcept { return {}; }
+    std::optional<Result> result_;
+    void
+    return_value (Result &&value) noexcept (std::is_nothrow_move_constructible_v<Result>)
+    {
+      result_.emplace (std::move (value));      // move-only
+    }
+    void
+    return_value (const Result &value) noexcept (std::is_nothrow_copy_constructible_v<Result>)
+    {
+      result_.emplace (value);                  // copyable
+    }
+  };
+  // -- Awaitable Protocol --
+  constexpr bool await_ready () const noexcept { return !frame_ || frame_.done(); }
+  Result
+  await_resume()
+  {
+    if (frame_.promise().exception_) ase_rethrow (frame_.promise().exception_);
+    return std::move (*frame_.promise().result_);
+  }
+  std::coroutine_handle<>
+  await_suspend (std::coroutine_handle<> waiter) noexcept
+  {
+    // assert SINGLE caller for this coroutine
+    ASE_ASSERT_RETURN (frame_.promise().continuation_ == nullptr, std::noop_coroutine());
+    frame_.promise().continuation_ = waiter; // next task to wake up when we co_return
+    return frame_.done() ? waiter : frame_;  // handle to resume immediately (symmetric transfer)
+  }
+  // -- Frame Handling ---
+  explicit CoTask (std::coroutine_handle<promise_type> h) : frame_ (h) {}
+  ~CoTask ()
+  {
+    ASE_RETURN_UNLESS (frame_);
+    ASE_ASSERT_RETURN (frame_.done()); // assert unless we start implementing cancelation
+    frame_.destroy();
+  }
+  /*ctor*/ CoTask    (CoTask &&o) noexcept : frame_ (o.frame_) { o.frame_ = nullptr; }
+  CoTask&  operator= (CoTask &&o) noexcept { std::swap (frame_, o.frame_); return *this; }
+  /*ctor*/ CoTask    (const CoTask&) = delete;  // no-copy
+  CoTask&  operator= (const CoTask&) = delete;  // no-copy
+protected:
+  std::coroutine_handle<promise_type> frame_; ///< Handle for this task
+};
+
+// TODO: should we rethrow instead of storing if continuation_ == nullptr? i.e. we're a root task
+
+/// Like CoTask<Result> without return type.
+template<>
+struct CoTask<void> {
+  struct promise_type : CoTaskAux::promise_base {
+    std::coroutine_handle<> continuation_ = nullptr;    // Waiter frame co_await-ing this CoTask
+    CoTask get_return_object() noexcept { return CoTask { std::coroutine_handle<promise_type>::from_promise (*this) }; }
+    CoTaskAux::FinalAwaiter<promise_type> final_suspend() noexcept { return {}; }
+    constexpr void return_void() noexcept {}            // Handle 'co_return;'
+  };
+  // -- Awaitable Protocol --
+  constexpr bool await_ready () const noexcept { return !frame_ || frame_.done(); }
+  void
+  await_resume()
+  {
+    if (frame_.promise().exception_) ase_rethrow (frame_.promise().exception_);
+  }
+  std::coroutine_handle<>
+  await_suspend (std::coroutine_handle<> waiter) noexcept
+  {
+    // assert SINGLE caller for this coroutine
+    ASE_ASSERT_RETURN (frame_.promise().continuation_ == nullptr, std::noop_coroutine());
+    frame_.promise().continuation_ = waiter; // next task to wake up when we co_return
+    return frame_.done() ? waiter : frame_;  // handle to resume immediately (symmetric transfer)
+  }
+  // -- Frame Handling ---
+  explicit CoTask    (std::coroutine_handle<promise_type> h) : frame_ (h) {}
+  /*dtor*/~CoTask    () { ASE_RETURN_UNLESS (frame_); ASE_ASSERT_RETURN (frame_.done()); frame_.destroy(); }
+  /*ctor*/ CoTask    (CoTask &&o) noexcept : frame_ (o.frame_) { o.frame_ = nullptr; }
+  CoTask&  operator= (CoTask &&o) noexcept { std::swap (frame_, o.frame_); return *this; }
+  /*ctor*/ CoTask    (const CoTask&) = delete;  // no-copy
+  CoTask&  operator= (const CoTask&) = delete;  // no-copy
+protected:
+  std::coroutine_handle<promise_type> frame_; ///< Handle for this task
+};
+using CoTaskVoid = CoTask<void>;
+
 } // Ase
