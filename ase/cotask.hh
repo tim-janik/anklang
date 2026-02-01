c569ee89854b253429e13edb54be97f99e7bce87
Tim Janik <timj@gnu.org>
2026-02-01 18:32:20 +0100

    ase/cotask.hh: add DetachedTask for fire-and-forget coroutines

--- ase/cotask.hh
+++ ase/cotask.hh
@@ -18,4 +18,18 @@ concept IsAwaitable = requires (T &t, std::coroutine_handle<> h)
   t.await_suspend (h);
 };
 
+/// Start a coroutine in fire-and-forget mode.
+struct DetachedTask {
+  // not-awaitable: bool await_ready   () { return true; }
+  // not-awaitable: void await_suspend (std::coroutine_handle<> h) {}
+  // not-awaitable: void await_resume  () {}
+  struct promise_type {
+    constexpr DetachedTask get_return_object()      { return DetachedTask {}; }
+    constexpr auto         initial_suspend()        { return std::suspend_never{}; } // immediate start
+    constexpr auto         final_suspend() noexcept { return std::suspend_never{}; } // auto .destroy()
+    constexpr void         return_void()            {}
+    constexpr void         unhandled_exception()    { ase_rethrow (std::current_exception()); } //  std::terminate(); }
+  };
+};
+
 } // Ase
