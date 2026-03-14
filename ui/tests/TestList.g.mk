5f87c729789f5cb56144ea69399f9fe8e9d81966
Tim Janik <timj@gnu.org>
2026-03-14 20:20:45 +0000

    ui/tests/project_test.ts: add project tests for creation, properties, tracks and playback

--- ui/tests/TestList.g.mk
+++ ui/tests/TestList.g.mk
@@ -1,4 +1,7 @@
 UI_TEST_LIST := \
+  project_test.test_project_basic \
+  project_test.test_project_master_volume \
+  project_test.test_project_track_management \
   util_test.test_string_ops \
   util_test.test_number_ops \
   util_test.test_array_ops \
