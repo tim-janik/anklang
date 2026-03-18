7225cb3c3dad0a517ee28d018734105832f44c0b
Tim Janik <timj@gnu.org>
2026-03-18 22:39:19 +0100

    ase/main.cc: disable STDIO_REDIRECT flag until we have run modes

--- ase/main.cc
+++ ase/main.cc
@@ -588,7 +588,7 @@ main (int argc, char *argv[])
 
     WebuiFlags webui_flags = main_app.headless ? WebuiFlags::HEADLESS : WebuiFlags::NONE;
     if (main_app.ui_tests.size())
-      webui_flags = webui_flags | WebuiFlags::STDIO_REDIRECT | WebuiFlags::CONSOLE_LOGS;
+      webui_flags = webui_flags | WebuiFlags::CONSOLE_LOGS; // WebuiFlags::STDIO_REDIRECT
     auto ereason = webui_start_browser (arg_ui_mode, main_loop, webui_url, [] () { main_loop->quit (0); }, webui_flags);
 
     if (ereason.error)
