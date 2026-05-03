3c61c6712418b0c15aa7d18f011822577f028e10
User <user@test.com>
2026-05-03 22:15:54 +0000

    ui/b/aboutdialog: port from Lit to SolidJS
    Replace the Lit <b-aboutdialog> custom element with a SolidJS
    AboutDialog component. The component uses createSignal for reactive
    info pairs, onMount for async data loading, and Dom.show_modal()
    for imperative dialog control.
    
    Shell template now imports AboutDialog directly instead of using
    the <b-aboutdialog> element.
    
    onMount: Defer dialog show until version data is loaded.
    Await about_pairs() before calling Dom.show_modal() so the dialog
    appears at full size immediately instead of starting small and
    growing when async IPC data arrives.

--- ui/b/aboutdialog.jsx
+++ ui/b/aboutdialog.jsx
@@ -1,101 +1,73 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 // @ts-check
 
-import { LitComponent, html, render, noChange, JsExtract, docs, ref } from '../little.js';
+/** @class B-AboutDialog
+ * SolidJS component that displays version information about Anklang.
+ *
+ * ### Props:
+ * *onClose*
+ * : Callback invoked when the Close button is activated.
+ */
+
+import { createSignal, onMount, For } from 'solid-js';
 import * as Util from "../util.js";
 import * as Dom from "../dom.js";
 
-// == HTML ==
-const HTML = (t, d) => html`
-<dialog class="floating-dialog [&:not([open])]:hidden" ${ref (h => t.dialog = h)}>
-  <div class="dialog-header">
-    About ANKLANG
-  </div>
-  <div class="grid max-w-full">
-    ${INFOS_HTML (t, d)}
-  </div>
-  <div class="dialog-footer">
-    <button class="button-xl" autofocus @click=${e => t.emit_close()} > Close </button>
-  </div>
-</dialog>`;
-const INFOS_HTML = (t, d) =>
-  t.info_pairs.map (p => html`
-    <span class="col-start-1 min-w-[15em] pr-2 text-right align-top font-bold"> ${p[0]} </span>
-    <span class="col-start-2 whitespace-pre-wrap break-words"> ${p[1]} </span>
-  `);
-
-// == SCRIPT ==
-
-/** @class BAboutDialog
- * @description
- * The <b-aboutdialog> element is a modal [dialog] that displays version information about Anklang.
- * ### Events:
- * *close*
- * : A *close* event is emitted once the "Close" button activated.
- */
-export class BAboutDialog extends LitComponent {
-  createRenderRoot() { return this; }
-  render() { return HTML (this, {}); }
-  static properties = {
-    shown:	{ type: Boolean },
-  };
-  constructor()
-  {
-    super();
-    this.shown = false;
-    this.info_pairs = [];
-    this.dialog = null;
-    this.completed_promise = null;
-  }
-  updated (changed_props)
-  {
-    let info_promise;
-    if (changed_props.has ('shown') && this.shown) {
-      const load_infos = async () => {
-	this.info_pairs = await about_pairs();
-	this.requestUpdate(); // showModal with info_pairs
-      };
-      info_promise = load_infos();
-    }
-    if (this.shown && !this.dialog.open && this.info_pairs.length > 0) {
-      Dom.show_modal (this.dialog, () => this.emit_close());
-      const old_completed_promise = this.completed_promise;
-      this.completed_promise = (async () => {
-	await Promise.all ([old_completed_promise, info_promise, this.updateComplete]);
-	this.completed_promise = null;
-      }) ();
-    }
-    if (!this.shown && this.dialog.open) {
-      this.close_dialog();
-      this.requestUpdate();
-    }
-  }
-  close_dialog (event = null)
-  {
-    Util.prevent_event (event);
-    this.dialog.close();
-    const old_completed_promise = this.completed_promise;
-    this.completed_promise = (async () => {
-      await Promise.all ([old_completed_promise, this.updateComplete]);
-      this.completed_promise = null;
-    }) ();
-    return this.completed_promise;
-  }
-  emit_close (event = null)
-  {
-    Util.prevent_event (event);
-    this.dispatchEvent (new CustomEvent ('close', { detail: {} }));
-  }
-  disconnectedCallback()
-  {
-    if (this.dialog.open) // ensure focus is restored
-      this.dialog.close();
-    super.disconnectedCallback();
-  }
+// == STYLE ==
+Extra_css`
+dialog.b-about-dialog {
+  margin: auto;
 }
-customElements.define ('b-aboutdialog', BAboutDialog);
+`;
 
-async function about_pairs() {
+// == Component ==
+export function AboutDialog (props)
+{
+  const [info_pairs, set_info_pairs] = createSignal ([]);
+  let dialogRef;
+
+  onMount (async () => {
+    const pairs = await about_pairs ();
+    set_info_pairs (pairs);
+    if (dialogRef)
+      Dom.show_modal (dialogRef, () => props.onClose?.());
+  });
+
+  const handleClose = () => {
+    props.onClose?.();
+  };
+
+  return (
+    <dialog
+      class="b-about-dialog floating-dialog"
+      ref={dialogRef}
+      onClose={handleClose}
+      exclusive={true}
+      bwidth="9em"
+      style="z-index: 93">
+      <div class="dialog-header">
+        About ANKLANG
+      </div>
+      <div class="grid max-w-full">
+        <For each={info_pairs()}>
+          {pair => (
+            <>
+              <span class="col-start-1 min-w-[15em] pr-2 text-right align-top font-bold">{pair[0]}</span>
+              <span class="col-start-2 whitespace-pre-wrap break-words">{pair[1]}</span>
+            </>
+          )}
+        </For>
+      </div>
+      <div class="dialog-footer">
+        <button class="button-xl" autofocus onClick={() => props.onClose?.()}>Close</button>
+      </div>
+    </dialog>
+  );
+};
+
+// == Data loading (unchanged) ==
+async function about_pairs()
+{
   const user_agent = navigator.userAgent.replace (/([)0-9]) ([A-Z])/gi, '$1\n$2');
   let array = [
     [ 'Anklang:',		CONFIG.version + ' (' + CONFIG.revdate.split (' ')[0] + ')' ],
@@ -108,18 +80,17 @@ async function about_pairs() {
   ];
   const Electron = window['Electron'];
   if (Electron)
-    {
-      const operating_system = Electron.platform + ' ' + Electron.arch + ' (' + Electron.os_release + ')';
-      const parray = [
-	[ 'Electron:',          Electron.versions.electron ],
-	[ 'Chrome:',            Electron.versions.chrome ],
-	[ 'Node.js:',           Electron.versions.node ],
-	[ 'Libuv:',             Electron.versions.uv ],
-	[ 'V8:',                Electron.versions.v8 ],
-	[ 'OS:',		operating_system ],
-      ];
-      array = Array.prototype.concat (array, parray);
-    }
+  {
+    const operating_system = Electron.platform + ' ' + Electron.arch + ' (' + Electron.os_release + ')';
+    const parray = [
+      [ 'Electron:',          Electron.versions.electron ],
+      [ 'Chrome:',            Electron.versions.chrome ],
+      [ 'Node.js:',           Electron.versions.node ],
+      [ 'Libuv:',             Electron.versions.uv ],
+      [ 'V8:',                Electron.versions.v8 ],
+      [ 'OS:',		operating_system ],
+    ];
+    array = Array.prototype.concat (array, parray);
+  }
   return array;
 }
-
