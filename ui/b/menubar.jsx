8ae0f53fc151a71b82f70ab2a73432d6a82e3901
User <user@test.com>
2026-05-06 02:27:35 +0000

    ui/b/menubar: port from Lit to SolidJS

--- ui/b/menubar.jsx
+++ ui/b/menubar.jsx
@@ -1,20 +1,23 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 // @ts-check
 
-import { LitComponent, html, JsExtract, live, docs, ref } from '../little.js';
-import * as Util from '../util.js';
-import * as Ase from '../../ase/gen/api-jsonipc.g.ts';
-import { hex, basename, dirname, displayfs, displaybasename, displaydirname } from '../strings.js';
-
-/** @class BMenuBar
- * @description
- * The <b-menubar> element contains main menus at the top of the window.
+/** @class B-MenuBar
+ * SolidJS component that displays the main menu bar with File, Edit, View, and Help menus.
+ *
+ * ### Props:
+ * *project*
+ * : The Ase.Project instance to monitor for dirty state changes.
  */
 
-// <STYLE/>
+import { createSignal, createEffect, onMount, onCleanup } from 'solid-js';
+import * as Util from "../util.js";
+import * as Ase from '../../ase/gen/api-jsonipc.g.ts';
+import { basename, dirname, displayfs, displaybasename, displaydirname } from '../strings.js';
+
+// == STYLE ==
 Extra_css`
 @reference "../tailwind.css";
-b-menubar {
+.b-menubar {
   .asbutton {
     @apply button-down-within size-10;
     position: relative;
@@ -23,10 +26,10 @@ b-menubar {
 .b-menubar-icon {
   position: absolute; inset: 0;
   display: inline-flex; justify-content: center;
-  [ic] { inset: 0; position: absolute; vertical-align: middle; } /* display: inline-block; */
+  [ic] { inset: 0; position: absolute; vertical-align: middle; }
 
   &::after {
-    content: '';		/* force rendering */
+    content: ''; 		/* force rendering */
     position: absolute; width: 0; height: 0;
     right: 0px;			/* Position at the right */
     bottom: 0px;		/* Position at the bottom */
@@ -34,143 +37,149 @@ b-menubar {
     border-right: 6px solid transparent;
     border-top: 6px solid white; /* Create a downward-pointing arrow */
     border-bottom: 0;
-    transform: rotate(-45deg);	/* Rotate to point into corner */
     transform-origin: 100% 100%;
-    transform: rotate(-45deg) translate(6px, -2px);
+    transform: rotate(-45deg) translate(6px, -2px); /* Rotate to point into corner */
   }
 }`;
 
-// <HTML/>
-const HTML = (t, d) =>  html`
-  <div class="hflex b-menubar m-2" style="justify-content: space-between" >
-    <!-- main menu & controlbar -->
+// == Component ==
+export function MenuBar (props)
+{
+  // refs for contextmenu elements
+  let filemenu, editmenu, viewmenu, helpmenu;
+  // track whether keyboard hotkeys have been mapped (once per mount)
+  let kbd_mapped = false;
+  // cleanup function for project notification
+  let project_cleanup = null;
 
-    <!-- menubar left -->
-    <b-buttonbar class="-menubar" >
-      <!-- File Menu -->
-      <div class="asbutton button-dim" data-tip="**CLICK** File Menu" data-hotkey="Alt+F" @click=${e => t.filemenu.popup (e)} @mousedown=${e => t.filemenu.popup (e)}
-	   id="g-filemenu" >
-	<div class="b-menubar-icon" >
-	  <b-icon ic="md-folder" ></b-icon>
-	</div>
-	<b-contextmenu ${ref (h => t.filemenu = h)} .activate=${activate} .isactive=${isactive} >
-	  <button ic="fa-file_o"	kbd="Ctrl+N"		uri="loadnew" >	New Project		</button>
-	  <button ic="fa-file_audio_o" 	kbd="Ctrl+O"		uri="load"    >	Open Project…		</button>
-	  <button ic="fa-save"     	kbd="Ctrl+S"		uri="save"    >	Save Project		</button>
-	  <button ic="fa-save"	   	kbd="Shift+Ctrl+S"	uri="saveas"  >	Save As…		</button>
-	  <b-menuseparator></b-menuseparator>
-	  <button ic="fa-cog"          	kbd="Ctrl+RawComma"	uri="prefs"   >	Preferences		</button>
-	  <b-menuseparator></b-menuseparator>
-	  <button ic="md-close"        	kbd="Shift+Ctrl+Q" 	uri="quit"    >	Quit			</button>
-	</b-contextmenu>
-      </div>
+  onMount (() => {
+    // subscribe to project dirty notifications
+    project_cleanup = Data.project.on ("notify:dirty", () => {
+      check_isactive();
+    });
+  });
 
-      <!-- Edit Menu -->
-      <div class="asbutton button-dim" data-tip="**CLICK** Edit Menu" data-hotkey="Alt+E" @click=${e => t.editmenu.popup (e)} @mousedown=${e => t.editmenu.popup (e)}
-	   id="g-editmenu" >
-	<div class="b-menubar-icon" >
-	  <b-icon ic="md-playlist_edit" ></b-icon>
-	</div>
-	<b-contextmenu ${ref (h => t.editmenu = h)}  .activate=${activate} .isactive=${isactive} >
-	  <button ic="md-undo" .disabled=${!true} kbd="Ctrl+Z"       uri="undo">	Undo	</button>
-	  <button ic="md-redo" .disabled=${!true} kbd="Shift+Ctrl+Z" uri="redo">	Redo	</button>
-	</b-contextmenu>
-      </div>
+  onCleanup (() => {
+    project_cleanup?.();
+    project_cleanup = null;
+    kbd_mapped = false;
+  });
 
-      <!-- View Menu -->
-      <div class="asbutton button-dim" data-tip="**CLICK** View Menu" data-hotkey="Alt+V" @click=${e => t.viewmenu.popup (e)} @mousedown=${e => t.viewmenu.popup (e)}
-	   id="g-viewmenu" >
-	<div class="b-menubar-icon" >
-	  <b-icon ic="fa-eye" ></b-icon>
-	</div>
-	<b-contextmenu ${ref (h => t.viewmenu = h)} .activate=${activate} .isactive=${isactive} >
-	  <button ic="md-fullscreen" .disabled=${!document.fullscreenEnabled}
-		  kbd="F11" uri="fullscreen">	Toggle Fullscreen	</button>
-	  ${ELECTRON_MENUITEMS (t)}
-	</b-contextmenu>
-      </div>
-
-    </b-buttonbar>
-
-    <!-- playcontrols -->
-    <div class="hflex">
-      <b-playcontrols></b-playcontrols>
-      <b-positionview></b-positionview>
-    </div>
-
-    <!-- menubar right -->
-    <b-buttonbar class="-menubar" >
-      <!-- Help Menu -->
-      <div class="asbutton button-dim" data-tip="**CLICK** Help Menu" data-hotkey="Alt+H" @click=${e => t.helpmenu.popup (e)} @mousedown=${e => t.helpmenu.popup (e)}
-	   id="g-helpmenu" >
-	<div class="b-menubar-icon" >
-	  <b-icon ic="fa-life_ring" ></b-icon>
-	</div>
-	<b-contextmenu ${ref (h => t.helpmenu = h)} .activate=${activate} .isactive=${isactive} >
-	  <button ic="fa-book_open"	uri="anklang-docu">	Anklang Documentation…	</button>
-	  <b-menuseparator></b-menuseparator>
-	  <button ic="oct-id_badge"		uri="about">		About…			</button>
-	</b-contextmenu>
-      </div>
-    </b-buttonbar>
-
-  </div>
-`;
-const ELECTRON_MENUITEMS = (t) => window['Electron'] && html`
-  <button ic="cod-zoom_in"  kbd="Ctrl++" uri="zoom-in">			Zoom In		</button>
-  <button ic="cod-zoom_out" kbd="Ctrl+-" uri="zoom-out">		Zoom Out	</button>
-  <button ic="cod-screen_full" kbd="Ctrl+0" uri="zoom-reset">		Reset Zoom	</button>
-`;
-
-// == SCRIPT ==
-export class BMenuBar extends LitComponent {
-  createRenderRoot() { return this; }
-  render()
-  {
-    return HTML (this, {});
-  }
-  constructor()
-  {
-    super();
-    this.kbd_mapped = false;
-    this.project_cleanup1 = null;
-    this.filemenu = null;
-    this.editmenu = null;
-    this.viewmenu = null;
-    this.helpmenu = null;
-  }
-  connectedCallback()
-  {
-    super.connectedCallback();
-    this.project_cleanup1 = Data.project.on ("notify:dirty", () => this.check_isactive());
-  }
-  disconnectedCallback()
-  {
-    super.disconnectedCallback();
-    this.project_cleanup1?.();
-    this.project_cleanup1 = null;
-  }
-  updated (changed_props)
-  {
-    if (!this.kbd_mapped && this.isConnected && this.filemenu) {
-      this.filemenu.map_kbd_hotkeys (true);
-      this.editmenu.map_kbd_hotkeys (true);
-      this.viewmenu.map_kbd_hotkeys (true);
-      this.helpmenu.map_kbd_hotkeys (true);
-      this.kbd_mapped = true;
+  // after first render, map keyboard hotkeys and check menu state
+  createEffect (() => {
+    if (!kbd_mapped && filemenu) {
+      filemenu.map_kbd_hotkeys (true);
+      editmenu.map_kbd_hotkeys (true);
+      viewmenu.map_kbd_hotkeys (true);
+      helpmenu.map_kbd_hotkeys (true);
+      kbd_mapped = true;
     }
-    this.check_isactive();
-  }
-  check_isactive()
-  {
-    this.filemenu.check_isactive();
-    this.editmenu.check_isactive();
-    this.viewmenu.check_isactive();
-    this.helpmenu.check_isactive();
-  }
-}
-customElements.define ('b-menubar', BMenuBar);
+    check_isactive();
+  });
 
+  const check_isactive = () => {
+    filemenu?.check_isactive();
+    editmenu?.check_isactive();
+    viewmenu?.check_isactive();
+    helpmenu?.check_isactive();
+  };
+
+  return (
+    <div class={props.class ? 'hflex b-menubar m-2 ' + props.class : 'hflex b-menubar m-2'} style="justify-content: space-between">
+      {/* menubar left */}
+      <b-buttonbar class="-menubar">
+        {/* File Menu */}
+        <div class="asbutton button-dim" data-tip="**CLICK** File Menu" data-hotkey="Alt+F"
+             onClick={e => filemenu.popup (e)}
+             onMouseDown={e => filemenu.popup (e)}
+             id="g-filemenu">
+          <div class="b-menubar-icon">
+            <b-icon ic="md-folder"></b-icon>
+          </div>
+          <b-contextmenu ref={filemenu} activate={activate} isactive={isactive}>
+            <button ic="fa-file_o"       kbd="Ctrl+N"         uri="loadnew">  New Project       </button>
+            <button ic="fa-file_audio_o"  kbd="Ctrl+O"         uri="load">    Open Project…       </button>
+            <button ic="fa-save"          kbd="Ctrl+S"         uri="save">    Save Project        </button>
+            <button ic="fa-save"           kbd="Shift+Ctrl+S"   uri="saveas">  Save As…            </button>
+            <b-menuseparator></b-menuseparator>
+            <button ic="fa-cog"            kbd="Ctrl+RawComma"  uri="prefs">   Preferences         </button>
+            <b-menuseparator></b-menuseparator>
+            <button ic="md-close"          kbd="Shift+Ctrl+Q"   uri="quit">    Quit                </button>
+          </b-contextmenu>
+        </div>
+
+        {/* Edit Menu */}
+        <div class="asbutton button-dim" data-tip="**CLICK** Edit Menu" data-hotkey="Alt+E"
+             onClick={e => editmenu.popup (e)}
+             onMouseDown={e => editmenu.popup (e)}
+             id="g-editmenu">
+          <div class="b-menubar-icon">
+            <b-icon ic="md-playlist_edit"></b-icon>
+          </div>
+          <b-contextmenu ref={editmenu} activate={activate} isactive={isactive}>
+            <button ic="md-undo" kbd="Ctrl+Z"        uri="undo">  Undo  </button>
+            <button ic="md-redo" kbd="Shift+Ctrl+Z"  uri="redo">  Redo  </button>
+          </b-contextmenu>
+        </div>
+
+        {/* View Menu */}
+        <div class="asbutton button-dim" data-tip="**CLICK** View Menu" data-hotkey="Alt+V"
+             onClick={e => viewmenu.popup (e)}
+             onMouseDown={e => viewmenu.popup (e)}
+             id="g-viewmenu">
+          <div class="b-menubar-icon">
+            <b-icon ic="fa-eye"></b-icon>
+          </div>
+          <b-contextmenu ref={viewmenu} activate={activate} isactive={isactive}>
+            <button ic="md-fullscreen" disabled={!document.fullscreenEnabled}
+                    kbd="F11" uri="fullscreen">  Toggle Fullscreen  </button>
+            {electron_menuitems()}
+          </b-contextmenu>
+        </div>
+      </b-buttonbar>
+
+      {/* playcontrols */}
+      <div class="hflex">
+        <b-playcontrols></b-playcontrols>
+        <b-positionview></b-positionview>
+      </div>
+
+      {/* menubar right */}
+      <b-buttonbar class="-menubar">
+        {/* Help Menu */}
+        <div class="asbutton button-dim" data-tip="**CLICK** Help Menu" data-hotkey="Alt+H"
+             onClick={e => helpmenu.popup (e)}
+             onMouseDown={e => helpmenu.popup (e)}
+             id="g-helpmenu">
+          <div class="b-menubar-icon">
+            <b-icon ic="fa-life_ring"></b-icon>
+          </div>
+          <b-contextmenu ref={helpmenu} activate={activate} isactive={isactive}>
+            <button ic="fa-book_open"   uri="anklang-docu">  Anklang Documentation…  </button>
+            <b-menuseparator></b-menuseparator>
+            <button ic="oct-id_badge"    uri="about">         About…                  </button>
+          </b-contextmenu>
+        </div>
+      </b-buttonbar>
+    </div>
+  );
+};
+
+// == Electron-specific menu items ==
+function electron_menuitems()
+{
+  if (!window['Electron'])
+    return null;
+  return (
+    <>
+      <button ic="cod-zoom_in"   kbd="Ctrl++"   uri="zoom-in">    Zoom In       </button>
+      <button ic="cod-zoom_out"  kbd="Ctrl+-"   uri="zoom-out">   Zoom Out      </button>
+      <button ic="cod-screen_full" kbd="Ctrl+0"  uri="zoom-reset"> Reset Zoom    </button>
+    </>
+  );
+};
+
+// == Menu activation handlers ==
 async function isactive (uri)
 {
   switch (uri) {
@@ -193,18 +202,18 @@ async function activate (uri, event)
       break;
     case 'quit':
       v = App.async_button_dialog ("Save Project?",
-				   "The current project may contain unsaved changes.\n" +
-				   "Save changes to the project before closing?",
-				   (Electron ?
-				    [ 'Discard Changes', { label: 'Cancel', autofocus: true }, 'Save' ] :
-				    [ 'Discard Changes', { label: 'Cancel', autofocus: true },
-				      { label: 'Save', disabled: false } ]),
-				   'QUESTION');
+                                   "The current project may contain unsaved changes.\n" +
+                                   "Save changes to the project before closing?",
+                                   (Electron ?
+                                    [ 'Discard Changes', { label: 'Cancel', autofocus: true }, 'Save' ] :
+                                    [ 'Discard Changes', { label: 'Cancel', autofocus: true },
+                                      { label: 'Save', disabled: false } ]),
+                                   'QUESTION');
       v = await v;
       if (v == 0)
-	return window_close();
+        return window_close();
       if (v == 2 && await save_project())
-	return window_close();
+        return window_close();
       break;
     case 'about':
       Shell.show_about_dialog (true);
@@ -227,9 +236,9 @@ async function activate (uri, event)
       break;
     case 'fullscreen':
       if (document.fullscreen)
-	document.exitFullscreen();
+        document.exitFullscreen();
       else
-	document.body.requestFullscreen();
+        document.body.requestFullscreen();
       break;
     case 'undo':
       await Data.project.undo();
@@ -267,18 +276,17 @@ async function open_file() {
     button: _('Open File'),
     cwd:    open_file_last_dir,
     filters: [ { name: 'Projects', extensions: ['anklang'] }, // TODO: filters
-	       { name: 'Audio Files', extensions: [ 'anklang', 'mid', 'wav', 'mp3', 'ogg' ] },
-	       { name: 'All Files', extensions: [ '*' ] }, ],
+               { name: 'Audio Files', extensions: [ 'anklang', 'mid', 'wav', 'mp3', 'ogg' ] },
+               { name: 'All Files', extensions: [ '*' ] }, ],
   };
   const filename = await Shell.select_file (opt);
   if (!filename)
     return;
   open_file_last_dir = dirname (filename);
   const err = await App.load_project_checked (filename);
-  if (err != Ase.Error.NONE)
-    {
-      // load_project_checked() displays a dialog
-    }
+  if (err != Ase.Error.NONE) {
+    // load_project_checked() displays a dialog
+  }
 }
 let open_file_last_dir = "~MUSIC";
 
@@ -297,18 +305,16 @@ async function save_project (asnew = false) {
   if (!filename)
     return false;
   save_project_last_dir = dirname (filename);
-  //if (!filename.endsWith ('.anklang'))
-  //  filename += '.anklang';
   if (0 && !replace) // TODO: if file_exists (filename)
     {
       replace = await App.async_button_dialog ("Replace Project?",
-					       "Replace existing project file?\n" +
-					       displayfs (filename) + ": File exists", [
-						 'Cancel',
-						 { label: 'Replace', autofocus: true }, ],
-					       'QUESTION');
+                                               "Replace existing project file?\n" +
+                                               displayfs (filename) + ": File exists", [
+                                                 'Cancel',
+                                                 { label: 'Replace', autofocus: true }, ],
+                                               'QUESTION');
       if (replace != 1)
-	return false;
+        return false;
     }
   let msg, err = await App.save_project (filename);
   if (err === Ase.Error.NONE) {
