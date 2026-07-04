ef99dce02c82ed3dd7aba9a061babd08c114b3c3
Tim Janik <timj@gnu.org>
2026-07-04 12:54:47 +0200

    ui/b/buttonbar.tsx: port to SolidJS

--- ui/b/buttonbar.tsx
+++ ui/b/buttonbar.tsx
@@ -1,19 +1,11 @@
 // This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
 // @ts-check
 
-import { LitComponent, html, JsExtract, docs } from '../little.js';
-
-/** @class BButtonBar
- * @description
- * The <b-buttonbar> element is a container for tight packing of buttons.
- * ### Slots:
- * *slot*
- * : All contents passed into this element will be packed tightly and styled as buttons.
- */
+import { type JSX, splitProps } from 'solid-js';
 
 // == STYLE ==
 Extra_css`
-b-buttonbar {
+.b-buttonbar {
   display: inline-flex; background-color: var(--b-button-border);
   border: 1px solid var(--b-button-border);
   border-radius: var(--b-button-radius);
@@ -25,14 +17,16 @@ b-buttonbar {
   }
 }`;
 
-// == HTML ==
-const HTML = html`
-  <slot></slot>
-`;
-
-// == SCRIPT ==
-class BButtonBar extends LitComponent {
-  createRenderRoot() { return this; }
-  render = () => HTML;
+// == COMPONENT ==
+/** @class ButtonBar
+ * @description
+ * The ButtonBar component is a container for tight packing of buttons.
+ * ### Children:
+ * : All children passed to this component will be packed tightly and styled as buttons.
+ */
+export function ButtonBar (props: JSX.HTMLAttributes<HTMLDivElement>)
+{
+  const [local, others] = splitProps (props, ['class', 'children']);
+  const class_name = 'b-buttonbar' + (local.class ? ' ' + local.class : '');
+  return <div class={class_name} {...others}>{local.children}</div>;
 }
-customElements.define ('b-buttonbar', BButtonBar);
