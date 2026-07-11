fbd33bf507d50b112870aa9b3495ab3a8a6a8196
Tim Janik <timj@gnu.org>
2026-07-11 05:28:48 +0200

    ui/Makefile.mk, ui/tests/aux.ts: remove unused aux.ts

--- ui/tests/aux.ts
+++
@@ -1,7 +0,0 @@
-// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
-
-/// Wait for a single frame (~17ms at 60Hz) for simple roundtrips
-export async function wait_frame (): Promise<void>
-{
-  await new Promise (r => setTimeout (r, 17));
-}
