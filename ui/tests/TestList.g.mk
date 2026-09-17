2c51bfe13ff9ed4cfc79218ee103c62659be7cc9
Tim Janik <timj@gnu.org>
2026-09-17 21:40:33 +0200

    ui/b/trackview.tsx: subscribe to levels once per mounted track

--- ui/tests/TestList.g.mk
+++ ui/tests/TestList.g.mk
@@ -34,4 +34,5 @@ UI_TEST_LIST := \
   track_test.test_track_mute \
   track_test.test_track_solo \
   track_test.test_track_midi_channel \
+  trackview_test.test_trackview_subscription \
   util_test.test_util \
