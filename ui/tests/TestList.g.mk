51540acfdb414459d8ffd4d97845d8fd6d748959
Tim Janik <timj@gnu.org>
2026-09-17 21:29:08 +0200

    ui/b/menuitems.tsx: describe popup contents as lists

--- ui/tests/TestList.g.mk
+++ ui/tests/TestList.g.mk
@@ -15,6 +15,7 @@ UI_TEST_LIST := \
   dialog_test.test_dialog \
   editable_test.test_editable \
   knob_test.test_knob \
+  menuitems_test.test_menuitems \
   modals_test.test_modals \
   numberinput_test.test_numberinput \
   objecteditor_test.test_objecteditor \
